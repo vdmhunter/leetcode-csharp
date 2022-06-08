7df098502847a86927fa586d254e0fc09e4e940f
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-08 22:21:56 +0300

    Add solution #02 for "1332. Remove Palindromic Subsequences"

---
+++ LeetCodeCSharpApp/1332-Remove Palindromic Subsequences/Solution-02.cs
@@ -0,0 +1,12 @@
+namespace LeetCodeCSharpApp.RemovePalindromicSubsequences02;
+
+public class Solution
+{
+    public int RemovePalindromeSub(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return 0;
+        var r = new string(s.ToCharArray().Reverse().ToArray());
+        return s == r ? 1 : 2;
+    }
+}
