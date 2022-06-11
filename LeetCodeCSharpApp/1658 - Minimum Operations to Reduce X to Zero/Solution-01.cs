c7bf6e097a829b9ee2fb46f1f35bbb9ea49cf841
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-12 00:20:29 +0300

    Add solution #01 for "1658. Minimum Operations to Reduce X to Zero"

---
+++ LeetCodeCSharpApp/1658 - Minimum Operations to Reduce X to Zero/Solution-01.cs
@@ -0,0 +1,50 @@
+namespace LeetCodeCSharpApp.MinimumOperationsToReduceXtoZero01;
+
+public class Solution
+{
+    public int MinOperations(int[] nums, int x)
+    {
+        var minStep = int.MaxValue;
+
+        var sum = nums.Sum();
+
+        //sum of the required subarray
+        var target = sum - x;
+
+        switch (target)
+        {
+            //if all the elements has to be selected
+            case 0:
+                return nums.Length;
+            //if even selecting all the elements aren't enough
+            case < 0:
+                return -1;
+        }
+
+        //to track the subarray/sliding window
+        int l = 0, r = 0;
+
+        //sum of the sliding window
+        var ps = nums[r];
+        do
+        {
+            if (ps >= target)
+            {
+                //if current subarray/window sum matches the target
+                //we update the ans, and continue to search the largest window
+                if (ps == target) minStep = Math.Min(minStep, nums.Length - (r - l + 1));
+                ps -= nums[l];
+                l++;
+            }
+            else
+            {
+                r++;
+
+                if (r < nums.Length)
+                    ps += nums[r];
+            }
+        } while (l < nums.Length && r < nums.Length);
+
+        return minStep != int.MaxValue ? minStep : -1;
+    }
+}
