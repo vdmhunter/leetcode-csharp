455a9a2fd3df8fc5c80b5fcd94a0ec91bbfed199
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-13 12:24:25 +0300

    Add solution #01 for "1695. Maximum Erasure Value"

---
+++ LeetCodeCSharpApp/1695 - Maximum Erasure Value/Solution-01.cs
@@ -0,0 +1,31 @@
+namespace LeetCodeCSharpApp.MaximumErasureValue01;
+
+public class Solution 
+{
+    public int MaximumUniqueSubarray(int[] nums)
+    {
+        var n = nums.Length;
+        var seen = new int[100_001]; 
+        
+        int left = 0, right = 0;
+        int maxScore = 0, currScore = 0;
+        
+        while (right < n)
+        {
+            seen[nums[right]]++;
+            currScore += nums[right];
+            // Keep moving left pointer until we get rid of duplicate element
+            while (right < n && seen[nums[right]] > 1)
+            {
+                seen[nums[left]]--;
+                currScore -= nums[left];
+                left++;
+            }
+            // Save best score
+            maxScore = Math.Max(maxScore, currScore);
+            right++;
+        }
+        
+        return maxScore;        
+    }
+}
