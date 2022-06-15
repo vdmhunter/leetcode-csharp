8a763304bfd7797a7ef1cc2528baa940bb2e1545
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-16 02:12:13 +0300

    Add solution #01 for "1048. Longest String Chain"

---
+++ LeetCodeCSharpApp/1048-Longest String Chain/Solution-01.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeCSharpApp.LongestStringChain01;
+
+public class Solution
+{
+    public int LongestStrChain(string[] words)
+    {
+        Array.Sort(words, (x, y) => x.Length.CompareTo(y.Length));
+        
+        var wordsLength = words.Distinct().ToDictionary(word => word, _ => 1);
+
+        var max = 0;
+        foreach (var word in words)
+        {
+            for (var i = 0; i < word.Length; i++)
+            {
+                var prevWord = word[..i] + word[(i + 1)..];
+                if (wordsLength.ContainsKey(prevWord))
+                    wordsLength[word] = Math.Max(wordsLength[word], wordsLength[prevWord] + 1);
+            }
+
+            max = Math.Max(max, wordsLength[word]);
+        }
+
+        return max;
+    }
+}
