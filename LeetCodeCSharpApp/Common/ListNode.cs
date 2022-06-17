dda20ba12b9e5798c2b38249400795ba4e2120e2
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-18 01:07:53 +0300

    Refactoring for ListNode

--- LeetCodeCSharpApp/Common/ListNode.cs
+++ LeetCodeCSharpApp/Common/ListNode.cs
@@ -1,10 +1,11 @@
+// ReSharper disable InconsistentNaming
+
 namespace LeetCodeCSharpApp.Common;
 
 public class ListNode
 {
-    // ReSharper disable once InconsistentNaming
+    
     public int val;
-    // ReSharper disable once InconsistentNaming
     public ListNode next;
 
     public ListNode(int val = 0, ListNode next = null!)
