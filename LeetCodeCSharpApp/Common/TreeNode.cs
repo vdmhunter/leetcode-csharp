7d6ce0a2e6af05e0d88a51680661a5912716504e
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-18 01:07:29 +0300

    Add TreeNode class

---
+++ LeetCodeCSharpApp/Common/TreeNode.cs
@@ -0,0 +1,19 @@
+// ReSharper disable InconsistentNaming
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace LeetCodeCSharpApp.Common;
+
+public class TreeNode
+{
+    
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null!, TreeNode right = null!)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
