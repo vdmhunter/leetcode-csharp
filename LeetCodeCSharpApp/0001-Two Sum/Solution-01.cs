8d40dcaea8bad1583431f3e0390ecae4b82c35c7
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-18 22:18:38 +0300

    Refactoring solution #01 for "1. Two Sum"

--- LeetCodeCSharpApp/0001-Two Sum/Solution-01.cs
+++ LeetCodeCSharpApp/0001-Two Sum/Solution-01.cs
@@ -5,11 +5,9 @@ public class Solution
     public int[]? TwoSum(int[] nums, int target)
     {
         for (var i = 0; i < nums.Length - 1; i++)
-        {
-            for (var j = i + 1; j < nums.Length; j++)
-                if (nums[i] + nums[j] == target)
-                    return new[] { i, j };
-        }
+        for (var j = i + 1; j < nums.Length; j++)
+            if (nums[i] + nums[j] == target)
+                return new[] { i, j };
 
         return null;
     }
