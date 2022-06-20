6378a4c809f1d74d61a57932b1c78594da508283
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-21 01:30:34 +0300

    Add solution #02 for "820. Short Encoding of Words"

---
+++ LeetCodeCSharpApp/0820-Short Encoding Of Words/Solution-02.cs
@@ -0,0 +1,28 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.ShortEncodingOfWords02;
+
+/// <summary>
+/// Trie
+/// </summary>
+public class Solution
+{
+    public int MinimumLengthEncoding(string[] words)
+    {
+        var trie = new TrieNode4();
+        var nodes = new Dictionary<TrieNode4, int>();
+
+        for (var i = 0; i < words.Length; ++i)
+        {
+            var word = words[i];
+            var cur = trie;
+            
+            for (var j = word.Length - 1; j >= 0; --j)
+                cur = cur.Get(word[j]);
+            
+            nodes.Add(cur, i);
+        }
+
+        return nodes.Keys.Where(node => node.Count == 0).Sum(node => words[nodes[node]].Length + 1);
+    }
+}
---
+++ LeetCodeCSharpApp/Common/TrieNode4.cs
@@ -0,0 +1,25 @@
+namespace LeetCodeCSharpApp.Common;
+
+public class TrieNode4
+{
+    private readonly TrieNode4[] _children;
+
+    public int Count;
+
+    public TrieNode4()
+    {
+        _children = new TrieNode4[26];
+        Count = 0;
+    }
+
+    public TrieNode4 Get(char c)
+    {
+        if (_children[c - 'a'] != null)
+            return _children[c - 'a'];
+
+        _children[c - 'a'] = new TrieNode4();
+        Count++;
+
+        return _children[c - 'a'];
+    }
+}
