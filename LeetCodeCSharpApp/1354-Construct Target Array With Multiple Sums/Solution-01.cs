d2e5f6e57e8664c594452d290973ffc93430992d
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-06-24 17:04:25 +0300

    Add solution #01 for "1354. Construct Target Array With Multiple Sums"

---
+++ LeetCodeCSharpApp/1354-Construct Target Array With Multiple Sums/Solution-01.cs
@@ -0,0 +1,35 @@
+namespace LeetCodeCSharpApp.ConstructTargetArrayWithMultipleSums01;
+
+/// <summary>
+/// Priority Queue
+/// </summary>
+public class Solution
+{
+    public bool IsPossible(int[] target)
+    {
+        var pq = new PriorityQueue<int, int>();
+        var total = 0L;
+        
+        foreach (var a in target)
+        {
+            total += a;
+            pq.Enqueue(a, -a);
+        }
+
+        while (true)
+        {
+            var a = pq.Dequeue();
+            total -= a;
+
+            if (a == 1 || total == 1)
+                return true;
+
+            if (a < total || total == 0 || a % total == 0)
+                return false;
+
+            a = Convert.ToInt32(a % total);
+            total += a;
+            pq.Enqueue(a, -a);
+        }
+    }
+}
