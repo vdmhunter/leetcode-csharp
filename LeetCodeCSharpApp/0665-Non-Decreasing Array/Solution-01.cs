a7c52bdd9f4d65b14522804bbc62645fd0a1f719
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-25 23:56:53 +0300

    Add solution #01 for "665. Non-decreasing Array"

---
+++ LeetCodeCSharpApp/0665-Non-Decreasing Array/Solution-01.cs
@@ -0,0 +1,22 @@
+namespace LeetCodeCSharpApp.NonDecreasingArray01;
+
+public class Solution
+{
+    public bool CheckPossibility(int[] nums)
+    {
+        var check = false;
+        
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] <= nums[i + 1])
+                continue;
+            
+            if (check || (i > 0 && i < nums.Length - 2 && nums[i + 2] < nums[i] && nums[i - 1] > nums[i + 1]))
+                return false;
+
+            check = true;
+        }
+
+        return true;
+    }
+}
