71702362aa40eff66b4e01689f15c781036ab100
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-06-26 03:55:00 +0300

    Add solution #01 for "1423. Maximum Points You Can Obtain from Cards"

---
+++ LeetCodeCSharpApp/1423-Maximum Points You Can Obtain From Cards/Solution-01.cs
@@ -0,0 +1,18 @@
+namespace LeetCodeCSharpApp.MaximumPointsYouCanObtainFromCards01;
+
+public class Solution
+{
+    public int MaxScore(int[] cardPoints, int k)
+    {
+        var total = cardPoints[..k].Sum();
+        var maxLength = total;
+        
+        for (var i = 0; i < k; i++)
+        {
+            total += cardPoints[cardPoints.Length - 1 - i] - cardPoints[k - 1 - i];
+            maxLength = Math.Max(maxLength, total);
+        }
+
+        return maxLength;
+    }
+}
