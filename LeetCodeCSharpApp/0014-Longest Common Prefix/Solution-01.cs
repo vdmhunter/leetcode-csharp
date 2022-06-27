7f082483c6821359159737cc863b9ccfa47e4bde
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-06-27 18:18:03 +0300

    Add solution #01 for "14. Longest Common Prefix"

---
+++ LeetCodeCSharpApp/0014-Longest Common Prefix/Solution-01.cs
@@ -0,0 +1,23 @@
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+
+namespace LeetCodeCSharpApp.LongestCommonPrefix01;
+
+public class Solution
+{
+    
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var result = string.Empty;
+        var shortest = strs.MinBy(s => s.Length); //strs.OrderBy(s => s.Length).FirstOrDefault()
+
+        foreach (var c in shortest!)
+        {
+            result += c;
+            if (!strs.All(s => s.StartsWith(result)))
+                return result[..^1];
+        }
+
+        return result;
+    }
+}
