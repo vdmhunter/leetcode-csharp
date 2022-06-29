7c9c198b56da89f632937fdb620b7662d43e5e08
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-06-30 00:49:39 +0300

    Add part of solution #01 for "406. Queue Reconstruction by Height"

---
+++ LeetCodeCSharpApp/0406-Queue Reconstruction By Height/Solution-01.cs
@@ -0,0 +1,11 @@
+namespace LeetCodeCSharpApp.QueueReconstructionByHeight01;
+
+public class Solution
+{
+    public int[][] ReconstructQueue(int[][] people)
+    {
+        var result = new int[people.Length][];
+
+        return result;
+    }
+}
