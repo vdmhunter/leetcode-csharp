9c20aa3cd813af7dd00f39ed4a1d039a13e1fa40
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-01 22:29:44 +0300

    Add solution #01 for "1710. Maximum Units on a Truck"

---
+++ LeetCodeCSharpApp/1710-Maximum Units On A Truck/Solution-01.cs
@@ -0,0 +1,23 @@
+namespace LeetCodeCSharpApp.MaximumUnitsOnATruck01;
+
+public class Solution
+{
+    public int MaximumUnits(int[][] boxTypes, int truckSize)
+    {
+        Array.Sort(boxTypes, (a, b) => b[1] - a[1]);
+
+        var result = 0;
+
+        foreach (var b in boxTypes)
+        {
+            var count = Math.Min(b[0], truckSize);
+            result += count * b[1];
+            truckSize -= count;
+            
+            if (truckSize == 0)
+                return result;
+        }
+
+        return result;
+    }
+}
