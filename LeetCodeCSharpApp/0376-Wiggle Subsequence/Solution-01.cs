df5a0db1d4f4c6bf32a41f4083486e651c1150c0
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-03 07:50:22 +0300

    Add solution #01 for "376. Wiggle Subsequence"

---
+++ LeetCodeCSharpApp/0376-Wiggle Subsequence/Solution-01.cs
@@ -0,0 +1,18 @@
+namespace LeetCodeCSharpApp.WiggleSubsequence01;
+
+public class Solution
+{
+    public int WiggleMaxLength(int[] nums)
+    {
+        if (nums.Length == 0)
+            return 0;
+
+        int up = 1, down = 1;
+        
+        for (var i = 1; i < nums.Length; i++)
+            if (nums[i] < nums[i - 1]) down = up + 1;
+            else if (nums[i] > nums[i - 1]) up = down + 1;
+        
+        return Math.Max(up, down);
+    }
+}
