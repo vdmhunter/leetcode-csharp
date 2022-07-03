5199804582e97c3348df09c74a096366c09172d2
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-03 07:27:17 +0300

    Add solution #01 for "2325. Decode the Message"

---
+++ LeetCodeCSharpApp/6108-Decode The Message/Solution-01.cs
@@ -0,0 +1,38 @@
+using System.Text;
+
+namespace LeetCodeCSharpApp.DecodeTheMessage01;
+
+public class Solution
+{
+    public string DecodeMessage(string key, string message)
+    {
+        var substitutionTable = new Dictionary<char, char>(26);
+
+        var currentLetter = 'a';
+        const char lastLetter = (char)('z' + 1);
+        
+        foreach (var c in key.Where(c => !substitutionTable.ContainsKey(c) && c != ' '))
+        {
+            substitutionTable[c] = currentLetter;
+            currentLetter = (char)(currentLetter + 1);
+            
+            if(currentLetter == lastLetter)
+                break;
+        }
+
+        var result = new StringBuilder();
+
+        foreach (var c in message)
+        {
+            if (c == ' ')
+            {
+                result.Append(' ');
+                continue;
+            }
+            
+            result.Append(substitutionTable[c]);
+        }
+        
+        return result.ToString();
+    }
+}
