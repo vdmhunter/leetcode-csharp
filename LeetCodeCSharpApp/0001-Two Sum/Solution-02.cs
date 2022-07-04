2a3dfa1be2d02c6ab304c0c54d420298b8f1baf3
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-07-04 16:31:11 +0300

    Add solution #02 for "1. Two Sum"

---
+++ LeetCodeCSharpApp/0001-Two Sum/Solution-02.cs
@@ -0,0 +1,28 @@
+namespace LeetCodeCSharpApp.TwoSum02;
+
+/// <summary>
+/// Dictionary
+/// </summary>
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var result = new int[2];
+        var map = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (map.ContainsKey(target - nums[i]))
+            {
+                result[0] = map[target - nums[i]];
+                result[1] = i;
+
+                return result;
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return result;
+    }
+}
