62d56cad13e261c550c1a6e895763aacd4886309
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-07-04 12:19:03 +0300

    Add solution #03 for "135. Candy"

---
+++ LeetCodeCSharpApp/0135-Candy/Solution-03.cs
@@ -0,0 +1,29 @@
+namespace LeetCodeCSharpApp.Candy03;
+
+/// <summary>
+/// Using one array
+/// </summary>
+public class Solution
+{
+    public int Candy(int[] ratings)
+    {
+        var candies = new int[ratings.Length];
+        
+        Array.Fill(candies, 1);
+
+        for (var i = 1; i < ratings.Length; i++)
+            if (ratings[i] > ratings[i - 1])
+                candies[i] = candies[i - 1] + 1;
+
+        var sum = candies[ratings.Length - 1];
+        for (var i = ratings.Length - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1])
+                candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+            
+            sum += candies[i];
+        }
+
+        return sum;
+    }
+}
