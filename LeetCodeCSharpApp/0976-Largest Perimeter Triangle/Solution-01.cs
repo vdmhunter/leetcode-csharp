99c5374319c759231849c5fe24e6ce0870c77b34
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-05 02:08:22 +0300

    Add solution #01 for "976. Largest Perimeter Triangle"

---
+++ LeetCodeCSharpApp/0976-Largest Perimeter Triangle/Solution-01.cs
@@ -0,0 +1,13 @@
+namespace LeetCodeCSharpApp.LargestPerimeterTriangle01;
+
+public class Solution
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        for (var i = nums.Length - 1; i > 1; --i)
+            if (nums[i] < nums[i - 1] + nums[i - 2])
+                return nums[i] + nums[i - 1] + nums[i - 2];
+        return 0;
+    }
+}
