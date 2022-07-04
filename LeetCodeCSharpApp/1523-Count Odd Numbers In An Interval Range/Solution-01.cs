a67689e1c9132bf9b8dbbf71e3301aca7704e490
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-07-04 16:00:01 +0300

    Add solution #01 for "1523. Count Odd Numbers in an Interval Range"

---
+++ LeetCodeCSharpApp/1523-Count Odd Numbers In An Interval Range/Solution-01.cs
@@ -0,0 +1,9 @@
+namespace LeetCodeCSharpApp.CountOddNumbersInAnIntervalRange01;
+
+public class Solution
+{
+    public int CountOdds(int low, int high)
+    {
+        return low % 2 == 0 && high % 2 == 0 ? (high - low) / 2 : (high - low) / 2 + 1;
+    }
+}
