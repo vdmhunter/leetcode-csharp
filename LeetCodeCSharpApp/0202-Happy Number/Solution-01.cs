f807910f492ed10676451fd57c5b2fcf2c56e9db
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-05 02:32:56 +0300

    Add solution #01 for "202. Happy Number"

---
+++ LeetCodeCSharpApp/0202-Happy Number/Solution-01.cs
@@ -0,0 +1,37 @@
+namespace LeetCodeCSharpApp.HappyNumber01;
+
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        int fast;
+        var slow = fast = n;
+        
+        do
+        {
+            slow = DigitSquareSum(slow);
+            fast = DigitSquareSum(fast);
+            fast = DigitSquareSum(fast);
+            
+            if (fast == 1)
+                return true;
+            
+        } while (slow != fast);
+
+        return false;
+    }
+
+    private int DigitSquareSum(int n)
+    {
+        var sum = 0;
+        
+        while (n > 0)
+        {
+            var tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
