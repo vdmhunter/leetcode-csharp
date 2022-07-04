b625e2e3e377d375e05f4e6f873f6c3b23ec236a
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-05 02:16:05 +0300

    Add solution #01 for "1779. Find Nearest Point That Has the Same X or Y Coordinate"

---
+++ LeetCodeCSharpApp/1779-Find Nearest Point That Has The Same X Or Y Coordinate/Solution-01.cs
@@ -0,0 +1,22 @@
+namespace LeetCodeCSharpApp.FindNearestPointThatHasTheSameXOrYCoordinate01;
+
+public class Solution
+{
+    public int NearestValidPoint(int x, int y, int[][] points)
+    {
+        var index = -1;
+        
+        for (int i = 0, smallest = int.MaxValue; i < points.Length; ++i)
+        {
+            int dx = x - points[i][0], dy = y - points[i][1];
+
+            if (dx * dy != 0 || Math.Abs(dy + dx) >= smallest)
+                continue;
+
+            smallest = Math.Abs(dx + dy);
+            index = i;
+        }
+
+        return index;
+    }
+}
