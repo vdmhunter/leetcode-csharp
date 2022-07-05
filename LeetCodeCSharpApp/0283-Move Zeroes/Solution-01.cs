d98e96a93cf6d8bc955b536ea2eed10a27463353
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-05 22:56:13 +0300

    Add solution #01 for "283. Move Zeroes"

---
+++ LeetCodeCSharpApp/0283-Move Zeroes/Solution-01.cs
@@ -0,0 +1,18 @@
+namespace LeetCodeCSharpApp.MoveZeroes01;
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var snowBallSize = 0;
+        
+        for (var i = 0; i < nums.Length; i++)
+            if (nums[i] == 0)
+                snowBallSize++;
+            else if (snowBallSize > 0)
+            {
+                nums[i - snowBallSize] = nums[i];
+                nums[i] = 0;
+            }
+    }
+}
