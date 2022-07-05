04ff648c2a96eaac3ffa62dd7fce4069ce996dd7
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-06 00:40:31 +0300

    Add solution #01 for "1356. Sort Integers by The Number of 1 Bits"

---
+++ LeetCodeCSharpApp/1356-Sort Integers By The Number Of 1 Bits/Solution-01.cs
@@ -0,0 +1,13 @@
+namespace LeetCodeCSharpApp.SortIntegersByTheNumberOf1Bits01;
+
+public class Solution
+{
+    public int[] SortByBits(int[] arr)
+    {
+        int NumberOf1(int i) => Convert.ToString(i, 2).Count(c => c == '1');
+
+        Array.Sort(arr, (a, b) => NumberOf1(a) - NumberOf1(b) == 0 ? a - b : NumberOf1(a) - NumberOf1(b));
+
+        return arr;
+    }
+}
