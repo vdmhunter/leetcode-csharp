eff15fcac0a1c318a23b77473f2661efe98220c6
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-06 00:48:53 +0300

    Add solution #01 for "232. Implement Queue using Stacks"

---
+++ LeetCodeCSharpApp/0232-Implement Queue Using Stacks/Solution-01.cs
@@ -0,0 +1,32 @@
+namespace LeetCodeCSharpApp.ImplementQueueUsingStacks01;
+
+public class MyQueue
+{
+    private readonly Stack<int> _s1 = new();
+    private readonly Stack<int> _s2 = new();
+
+    public void Push(int x)
+    {
+        while (_s1.Count != 0)
+            _s2.Push(_s1.Pop());
+        _s1.Push(x);
+        
+        while (_s2.Count != 0)
+            _s1.Push(_s2.Pop());
+    }
+
+    public int Pop()
+    {
+        return _s1.Pop();
+    }
+
+    public int Peek()
+    {
+        return _s1.Peek();
+    }
+
+    public bool Empty()
+    {
+        return _s1.Count == 0;
+    }
+}
