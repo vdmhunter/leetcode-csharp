60234b1607096332641050e7866d3b1cd394320a
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-06 01:16:52 +0300

    Fix solution #01 for "1480. Running Sum of 1d Array"

--- LeetCodeCSharpApp/1480-Running Sum Of 1D Array/Solution-01.cs
+++ LeetCodeCSharpApp/1480-Running Sum Of 1D Array/Solution-01.cs
@@ -4,21 +4,11 @@ public class Solution
 {
     public int[] RunningSum(int[] nums)
     {
-        var timestamp = (int)Math.Truncate(Math.Log2(nums.Length)) + 1;
-        var res = new int[timestamp + 1][];
-        res[0] = nums;
+        var result = (int[])nums.Clone();
+            
+        for (var i = 0; i < nums.Length; i++)
+            result[i] = nums[..i].Sum();
 
-        for (var i = 0; i < timestamp; i++)
-        {
-            res[i + 1] = new int[nums.Length];
-
-            for (var j = 0; j < nums.Length; j++)
-                if (j < (int)Math.Pow(2, i))
-                    res[i + 1][j] = res[i][j];
-                else
-                    res[i + 1][j] = res[i][j] + res[i][j - (int)Math.Pow(2, i)];
-        }
-
-        return res[timestamp];
+        return result;
     }
 }
