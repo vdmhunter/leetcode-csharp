73f984d8e5162c862dc7398f170a9d70191ef656
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-16 21:08:02 +0300

    Add solution #01 for "576. Out of Boundary Paths"

---
+++ LeetCodeCSharpApp/0576-Out Of Boundary Paths/Solution-01.cs
@@ -0,0 +1,21 @@
+namespace LeetCodeCSharpApp.OutOfBoundaryPaths01;
+
+/// <summary>
+/// Brute Force
+/// </summary>
+public class Solution
+{
+    public int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
+    {
+        if (startRow == m || startColumn == n || startRow < 0 || startColumn < 0)
+            return 1;
+
+        if (maxMove == 0)
+            return 0;
+
+        return FindPaths(m, n, maxMove - 1, startRow - 1, startColumn)
+               + FindPaths(m, n, maxMove - 1, startRow + 1, startColumn)
+               + FindPaths(m, n, maxMove - 1, startRow, startColumn - 1)
+               + FindPaths(m, n, maxMove - 1, startRow, startColumn + 1);
+    }
+}
