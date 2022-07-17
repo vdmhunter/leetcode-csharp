ce62f251ea56e39f5f62acd91c4e2ca07dd42e88
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-17 09:27:51 +0300

    Add solution #01 for "2341. Maximum Number of Pairs in Array"

---
+++ LeetCodeCSharpApp/2341-Maximum Number Of Pairs In Array/Solution-01.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeCSharpApp.MaximumNumberOfPairsInArray01;
+
+public class Solution
+{
+    public int[] NumberOfPairs(int[] nums)
+    {
+        var list = new List<int>(nums);
+        var count = 0;
+        
+        for (var i = list.Count; i > 0; --i)
+        {
+            var n = list[^i];
+            var idx = list.IndexOf(n, list.Count - i + 1);
+            
+            if (idx == -1)
+                continue;
+            
+            list.RemoveAt(idx);
+            list.RemoveAt(0);
+            i -= 1;
+            count++;
+        }
+
+        return new[] { count, list.Count };
+    }
+}
