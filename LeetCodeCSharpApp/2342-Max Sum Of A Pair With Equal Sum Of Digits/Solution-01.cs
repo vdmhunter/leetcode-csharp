0a75439008daf5144c80d0e3ddbd358b70ecc7aa
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-17 09:28:26 +0300

    Add solution #01 for "2342. Max Sum of a Pair With Equal Sum of Digits"

---
+++ LeetCodeCSharpApp/2342-Max Sum Of A Pair With Equal Sum Of Digits/Solution-01.cs
@@ -0,0 +1,24 @@
+namespace LeetCodeCSharpApp.MaxSumOfAPairWithEqualSumOfDigits01;
+
+public class Solution
+{
+    public int MaximumSum(int[] nums)
+    {
+        var dic = new Dictionary<int, List<int>>();
+
+        foreach (var n in nums)
+        {
+            var sum = n.ToString().Sum(c => c - '0');
+
+            if (!dic.ContainsKey(sum))
+                dic.Add(sum, new List<int> { n });
+            else
+                dic[sum].Add(n);
+        }
+
+        var pairsLists = dic.Values.Where(v => v.Count >= 2).ToList();
+
+        return pairsLists.Select(pairsList => pairsList.OrderByDescending(pl => pl).Take(2).Sum())
+            .Prepend(-1).Max();
+    }
+}
