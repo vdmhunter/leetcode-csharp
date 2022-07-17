f28dcb64f809e67b1650eecac0ecc884d39f6ab3
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-17 19:57:25 +0300

    Add batch script file for creation subfolders

---
+++ LeetCodeCSharpApp/Tools/CreateFolders.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal enabledelayedexpansion
+
+for /l %%n in (1,70,2500) do (
+  set /a k=%%n+69
+  set "formattedN=000000%%n"
+  set "formattedK=000000!k!"
+  rem echo !formattedN:~-4!-!formattedK:~-4! >> folders.txt
+  mkdir "[!formattedN:~-4!-!formattedK:~-4!]"
+)
+
+
