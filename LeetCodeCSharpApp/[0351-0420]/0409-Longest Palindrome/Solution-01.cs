0a65fcb6f4324134bec871af2de3157a2a938d34
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-18 22:07:03 +0300

    Add solution #01 for "409. Longest Palindrome"

---
+++ LeetCodeCSharpApp/[0351-0420]/0409-Longest Palindrome/Solution-01.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeCSharpApp.LongestPalindrome01;
+
+public class Solution
+{
+    public int LongestPalindrome(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return 0;
+        
+        var hs = new HashSet<char>();
+        var count = 0;
+        
+        foreach (var c in s)
+            if (hs.Contains(c))
+            {
+                hs.Remove(c);
+                count++;
+            }
+            else
+                hs.Add(c);
+
+        if (hs.Count != 0)
+            return count * 2 + 1;
+        
+        return count * 2;
+    }
+}
