d776132ba30728105953860287db53dc824be908
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-19 01:21:50 +0300

    Add solution #01 for "1046. Last Stone Weight"

---
+++ LeetCodeCSharpApp/[0981-1050]/1046-Last Stone Weight/Silution-01.cs
@@ -0,0 +1,20 @@
+namespace LeetCodeCSharpApp.LastStoneWeight01;
+
+public class Solution
+{
+    public int LastStoneWeight(int[] stones)
+    {
+        var pq = new PriorityQueue<int, int>();
+        
+        foreach (var s in stones)
+            pq.Enqueue(s, -s);
+        
+        while (pq.Count > 1)
+        {
+            var s = pq.Dequeue() - pq.Dequeue();
+            pq.Enqueue(s, -s);
+        }
+
+        return pq.Dequeue();
+    }
+}
