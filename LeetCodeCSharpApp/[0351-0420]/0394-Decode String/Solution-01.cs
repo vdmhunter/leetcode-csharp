6873bafc4f11269dd0df953fcf3214840f49dc05
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-19 01:12:13 +0300

    Add solution #01 for "394. Decode String"

---
+++ LeetCodeCSharpApp/[0351-0420]/0394-Decode String/Solution-01.cs
@@ -0,0 +1,43 @@
+using System.Text;
+
+namespace LeetCodeCSharpApp.DecodeString01;
+
+public class Solution
+{
+    private int _pos;
+    
+    public string DecodeString(string s)
+    {
+        var res = new StringBuilder();
+
+        for (var num = 0; _pos < s.Length; ++_pos)
+        {
+            var c = s[_pos];
+
+            if (char.IsDigit(c))
+                num = num * 10 + (c - '0');
+            else switch (c)
+            {
+                case '[':
+                {
+                    ++_pos;
+                
+                    var str = DecodeString(s);
+                
+                    for (var j = 0; j < num; ++j)
+                        res.Append(str);
+                
+                    num = 0;
+                    break;
+                }
+                case ']':
+                    return res.ToString();
+                default:
+                    res.Append(c);
+                    break;
+            }
+        }
+
+        return res.ToString();
+    }
+}
