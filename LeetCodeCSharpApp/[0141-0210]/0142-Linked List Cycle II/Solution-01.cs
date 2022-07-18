2721b78c202e46fe4573df8ff96ce986c51f0841
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-18 21:57:18 +0300

    Add solution #01 for "142. Linked List Cycle II"

---
+++ LeetCodeCSharpApp/[0141-0210]/0142-Linked List Cycle II/Solution-01.cs
@@ -0,0 +1,35 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.LinkedListCycleII01;
+
+public class Solution
+{
+    public ListNode DetectCycle(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+
+        //while (fast != null && fast.next != null)
+        //while (fast is { next: { } })
+        while (fast?.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast != slow)
+                continue;
+            
+            var slow2 = head;
+            
+            while (slow2 != slow)
+            {
+                slow = slow.next;
+                slow2 = slow2.next;
+            }
+
+            return slow;
+        }
+
+        return null!;
+    }
+}
