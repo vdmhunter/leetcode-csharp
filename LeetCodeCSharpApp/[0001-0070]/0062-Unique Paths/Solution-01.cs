883fb6bdc3970cc6736e373f3633abf48838ff5c
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-18 23:21:43 +0300

    Add solution #01 for "62. Unique Paths"

---
+++ LeetCodeCSharpApp/[0001-0070]/0062-Unique Paths/Solution-01.cs
@@ -0,0 +1,17 @@
+namespace LeetCodeCSharpApp.UniquePaths01;
+
+public class Solution
+{
+    public int UniquePaths(int m, int n)
+    {
+        var dp = new int[m, n];
+
+        for (var i = 0; i < m; i++)
+            for (var j = 0; j < n; j++)
+                dp[i, j] = i == 0 || j == 0
+                    ? 1 
+                    : dp[i - 1, j] + dp[i, j - 1];
+
+        return dp[m - 1, n - 1];
+    }
+}
