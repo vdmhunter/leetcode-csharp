099f25556e8e6fb1c511dd51f2e96bca1410c706
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-07-20 19:19:21 +0300

    Add solution #01 for "43. Multiply Strings"

---
+++ LeetCodeCSharpApp/[0001-0070]/0043-Multiply Strings/Solution-01.cs
@@ -0,0 +1,32 @@
+using System.Text;
+
+namespace LeetCodeCSharpApp.MultiplyStrings01;
+
+public class Solution
+{
+    public string Multiply(string num1, string num2)
+    {
+        int m = num1.Length, n = num2.Length;
+        var pos = new int[m + n];
+
+        for (var i = m - 1; i >= 0; i--)
+            for (var j = n - 1; j >= 0; j--)
+            {
+                var mul = (num1[i] - '0') * (num2[j] - '0');
+                var p1 = i + j;
+                var p2 = i + j + 1;
+                var sum = mul + pos[p2];
+
+                pos[p1] += sum / 10;
+                pos[p2] = sum % 10;
+            }
+
+        var sb = new StringBuilder();
+        
+        foreach (var p in pos)
+            if (!(sb.Length == 0 && p == 0))
+                sb.Append(p);
+        
+        return sb.Length == 0 ? "0" : sb.ToString();
+    }
+}
