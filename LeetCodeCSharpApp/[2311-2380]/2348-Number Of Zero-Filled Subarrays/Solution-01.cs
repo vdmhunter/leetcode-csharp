2a3a71683ff9f7913d2f4b4513f796ff64e3f597
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-23 19:37:28 +0300

    Add solution #01 for "2348. Number of Zero-Filled Subarrays"

---
+++ LeetCodeCSharpApp/[2311-2380]/2348-Number Of Zero-Filled Subarrays/Solution-01.cs
@@ -0,0 +1,28 @@
+namespace LeetCodeCSharpApp.NumberOfZeroFilledSubarrays01;
+
+public class Solution
+{
+    public long ZeroFilledSubarray(int[] nums)
+    {
+        var result = 0L;
+        var countOfZeros = 0L;
+
+        foreach (var n in nums)
+        {
+            if (n == 0)
+            {
+                countOfZeros++;
+                continue;
+            }
+                
+            if (countOfZeros != 0)
+                result += (1 + countOfZeros) * countOfZeros / 2;
+
+            countOfZeros = 0;
+        }
+		
+        result += (1 + countOfZeros) * countOfZeros / 2;
+
+        return result;
+    }
+}
