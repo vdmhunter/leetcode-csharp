3fc8a98ad30c23269aaf87220596d37a6541031f
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-07-25 19:37:56 +0300

    Add solution #01 for "34. Find First and Last Position of Element in Sorted Array"

---
+++ LeetCodeCSharpApp/[0001-0070]/0034-Find First And Last Position Of Element In Sorted Array/Solution-01.cs
@@ -0,0 +1,19 @@
+namespace LeetCodeCSharpApp.FindFirstAndLastPositionOfElementInSortedArray01;
+
+public class Solution
+{
+    public int[] SearchRange(int[] nums, int target)
+    {
+        var leftComparer = Comparer<int>.Create((a, b) => a == b ? 1 : a.CompareTo(b));
+        var rightComparer = Comparer<int>.Create((a, b) => a == b ? -1 : a.CompareTo(b));
+        
+        var left = ~Array.BinarySearch(nums, target, leftComparer);
+        var right = ~Array.BinarySearch(nums, target, rightComparer) - 1; 
+        
+        return new[]
+        {
+            left < nums.Length && nums[left] == target ? left : -1,
+            right >= 0 && nums[right] == target ? right : -1
+        };
+    }
+}
