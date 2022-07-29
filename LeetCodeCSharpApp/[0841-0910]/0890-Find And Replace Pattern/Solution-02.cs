5c89c77565d3c3bd4150eabe8e0bce2cab4f96c2
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-30 02:25:54 +0300

    Add solution #02 for "890. Find and Replace Pattern"

---
+++ LeetCodeCSharpApp/[0841-0910]/0890-Find And Replace Pattern/Solution-02.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeCSharpApp.FindAndReplacePattern02;
+
+public class Solution
+{
+    public IList<string> FindAndReplacePattern(string[] words, string pattern)
+    {
+        var p = F(pattern);
+
+        return words.Where(w => F(w).SequenceEqual(p)).ToList();
+    }
+
+    private static int[] F(string w)
+    {
+        var m = new Dictionary<char, int>();
+        var n = w.Length;
+        var result = new int[n];
+        
+        for (var i = 0; i < n; i++)
+        {
+            m.TryAdd(w[i], m.Count);
+            result[i] = m[w[i]];
+        }
+
+        return result;
+    }
+}
