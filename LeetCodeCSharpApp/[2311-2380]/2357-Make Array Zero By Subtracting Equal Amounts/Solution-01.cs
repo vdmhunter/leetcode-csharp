7e425c15daeb79f4e8997cd5a3e87e6d32437f44
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-07-31 13:14:27 +0300

    Add solution #01 for "2357. Make Array Zero by Subtracting Equal Amounts"

---
+++ LeetCodeCSharpApp/[2311-2380]/2357-Make Array Zero By Subtracting Equal Amounts/Solution-01.cs
@@ -0,0 +1,23 @@
+namespace LeetCodeCSharpApp.MakeArrayZeroBySubtractingEqualAmounts01;
+
+public class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        var n = 0;
+
+        while (true)
+        {
+            nums = nums.Where(i => i > 0).ToArray();
+            
+            if (nums.Length == 0)
+                break;
+
+            var min = nums.Min();
+            nums = nums.Select(i => i - min < 0 ? 0 : i - min).ToArray();
+            n++;
+        }
+
+        return n;
+    }
+}
