4cbd0debf6313c9e94590670f405bbf8631f7fba
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-08 00:50:25 +0300

    Add solution #01 for "1220. Count Vowels Permutation"

---
+++ LeetCodeCSharpApp/[1191-1260]/1220-Count Vowels Permutation/Solution-01.cs
@@ -0,0 +1,23 @@
+namespace LeetCodeCSharpApp.CountVowelsPermutation01;
+
+public class Solution
+{
+    private const ulong Mod = 1000000007;
+
+    public int CountVowelPermutation(int n)
+    {
+        ulong a = 1, e = 1, i = 1, o = 1, u = 1;
+
+        for (var iter = 1; iter < n; iter++)
+        {
+            ulong nextA = e + i + u, nextE = a + i, nextI = e + o, nextO = i, nextU = i + o;
+            a = nextA % Mod;
+            e = nextE % Mod;
+            i = nextI % Mod;
+            o = nextO % Mod;
+            u = nextU % Mod;
+        }
+
+        return (int)((a + e + i + o + u) % Mod);
+    }
+}
