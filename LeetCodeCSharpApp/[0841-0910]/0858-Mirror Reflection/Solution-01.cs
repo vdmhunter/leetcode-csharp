834ee60edd25e4c4e45f4977450de36c5efc38fe
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-08 00:51:36 +0300

    Add solution #01 for "858. Mirror Reflection"

---
+++ LeetCodeCSharpApp/[0841-0910]/0858-Mirror Reflection/Solution-01.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeCSharpApp.MirrorReflection01;
+
+public class Solution
+{
+    public int MirrorReflection(int p, int q)
+    {
+        int m = 1, n = 1;
+        
+        while (m * p != n * q)
+        {
+            n++;
+            m = n * q / p;
+        }
+
+        if (m % 2 == 0 && n % 2 == 1)
+            return 0;
+        
+        if (m % 2 == 1 && n % 2 == 1)
+            return 1;
+        
+        if (m % 2 == 1 && n % 2 == 0)
+            return 2;
+        
+        return -1;
+    }
+}
