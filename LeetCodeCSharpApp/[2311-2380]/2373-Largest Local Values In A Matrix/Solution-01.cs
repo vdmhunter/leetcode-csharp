7128ce9f0b4a25b757db0423b561efc287cf8614
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-14 22:50:50 +0300

    Add solution #01 for "2373. Largest Local Values in a Matrix"

---
+++ LeetCodeCSharpApp/[2311-2380]/2373-Largest Local Values In A Matrix/Solution-01.cs
@@ -0,0 +1,31 @@
+namespace LeetCodeCSharpApp.LargestLocalValuesInAMatrix01;
+
+public class Solution
+{
+    public int[][] LargestLocal(int[][] grid)
+    {
+        var n = grid.Length;
+        var result = new int[n - 2][];
+        
+        for (var i = 1; i < n - 1; i++)
+        {
+            result[i - 1] = new int[n - 2];
+            
+            for (var j = 1; j < n - 1; j++)
+                result[i - 1][j - 1] = MaxLocal(i, j);
+        }
+
+        int MaxLocal(int i, int j)
+        {
+            var max = 0;
+            
+            for (var ii = i - 1; ii <= i + 1; ii++)
+                for (var jj = j - 1; jj <= j + 1; jj++)
+                    max = Math.Max(max, grid[ii][jj]);
+
+            return max;
+        }
+
+        return result;
+    }
+}
