e96173edf0533aea6e39ea671fae919ad828390b
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-14 23:36:39 +0300

    Add solution #02 for "2375. Construct Smallest Number From DI String"

---
+++ LeetCodeCSharpApp/[2311-2380]/2375-Construct Smallest Number From DI String/Solution-02.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeCSharpApp.ConstructSmallestNumberFromDIString02;
+
+public class Solution
+{
+    public string SmallestNumber(string pattern)
+    {
+        var n = pattern.Length + 1;
+        var result = new char[n];
+        
+        for (var i = 0; i < n; i++) 
+            result[i] = (char)('1' + i);
+
+        var lastI = 0;
+        
+        for (var i = 0; i < n - 1; i++)
+            if (pattern[i] == 'I')
+            {
+                Array.Reverse(result, lastI, i + 1 - lastI);
+                lastI = i + 1;
+            }
+
+        if (pattern.Last() == 'D')
+            Array.Reverse(result, lastI, n - lastI);
+        
+        return new string(result);
+    }
+}
