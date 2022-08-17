c633d880ea98ca4d6c7d32a116a54b3c75209901
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-18 00:55:14 +0300

    Add solution #01 for "804. Unique Morse Code Words"

---
+++ LeetCodeCSharpApp/[0771-0840]/0804-Unique Morse Code Words/Solution-01.cs
@@ -0,0 +1,29 @@
+namespace LeetCodeCSharpApp.UniqueMorseCodeWords01;
+
+public class Solution
+{
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        var morseCodes = new Dictionary<char, string>
+        {
+            { 'a', ".-"   }, { 'b', "-..." }, { 'c', "-.-." }, { 'd', "-.."  },
+            { 'e', "."    }, { 'f', "..-." }, { 'g', "--."  }, { 'h', "...." },
+            { 'i', ".."   }, { 'j', ".---" }, { 'k', "-.-"  }, { 'l', ".-.." },
+            { 'm', "--"   }, { 'n', "-."   }, { 'o', "---"  }, { 'p', ".--." },
+            { 'q', "--.-" }, { 'r', ".-."  }, { 's', "..."  }, { 't', "-"    },
+            { 'u', "..-"  }, { 'v', "...-" }, { 'w', ".--"  }, { 'x', "-..-" },
+            { 'y', "-.--" }, { 'z', "--.." }
+
+        };
+
+        var transformations = new HashSet<string>();
+
+        foreach (var word in words)
+        {
+            var morseWord = word.Aggregate("", (current, next) => current + morseCodes[next]);
+            transformations.Add(morseWord);
+        }
+
+        return transformations.Count;
+    }
+}
