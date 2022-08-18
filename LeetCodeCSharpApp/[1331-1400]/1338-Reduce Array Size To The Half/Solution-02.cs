7621c2e46007fd267a8e368c430c91bd85062308
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-19 00:07:59 +0300

    Add solution #02 for "1338. Reduce Array Size to The Half"

---
+++ LeetCodeCSharpApp/[1331-1400]/1338-Reduce Array Size To The Half/Solution-02.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeCSharpApp.ReduceArraySizeToTheHalf02;
+
+public class Solution
+{
+    public int MinSetSize(int[] arr)
+    {
+        var d = new Dictionary<int, int>();
+
+        foreach (var i in arr)
+            if (!d.ContainsKey(i))
+                d[i] = 1;
+            else
+                d[i]++;
+
+        var l = d.Values.ToList();
+        l.Sort((x, y) => y.CompareTo(x));
+
+        var minTarget = arr.Length / 2;
+        var sum = 0;
+        
+        for (var i = 0; i < l.Count; i++)
+            if ((sum += l[i]) >= minTarget)
+                return i + 1;
+
+        return minTarget; 
+    }
+}
