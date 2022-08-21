8d01d69522856f007e18cd261a4c5a5c571976ef
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-21 19:06:28 +0300

    Add solution #01 for "2383. Minimum Hours of Training to Win a Competition"

---
+++ LeetCodeCSharpApp/[2381-2450]/2383-Minimum Hours Of Training To Win A Competition/Solution-01.cs
@@ -0,0 +1,37 @@
+namespace LeetCodeCSharpApp.MinimumHoursOfTrainingToWinACompetition01;
+
+public class Solution
+{
+    public int MinNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience)
+    {
+        var currentEnergy = initialEnergy;
+        var currentExperience = initialExperience;
+        var totalHours = 0;
+        
+        for (var i = 0; i < energy.Length; i++)
+        {
+            if (currentEnergy <= energy[i])
+            {
+                var hours = energy[i] - currentEnergy + 1;
+                totalHours += hours;
+                currentEnergy += hours;
+            }
+
+            currentEnergy -= energy[i];
+        }
+
+        for (var i = 0; i < experience.Length; i++)
+        {
+            if (currentExperience <= experience[i])
+            {
+                var hours = experience[i] - currentExperience + 1;
+                totalHours += hours;
+                currentExperience += hours;
+            }
+
+            currentExperience += experience[i];
+        }
+
+        return totalHours;
+    }
+}
