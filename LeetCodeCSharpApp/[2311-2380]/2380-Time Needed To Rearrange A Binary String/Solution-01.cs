7975945ccfc8dd07fd8cd084ba7356aa9cca9bee
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-21 19:08:54 +0300

    Add solution #01 for "2380. Time Needed to Rearrange a Binary String"

---
+++ LeetCodeCSharpApp/[2311-2380]/2380-Time Needed To Rearrange A Binary String/Solution-01.cs
@@ -0,0 +1,17 @@
+namespace LeetCodeCSharpApp.TimeNeededToRearrangeABinaryString01;
+
+public class Solution
+{
+    public int SecondsToRemoveOccurrences(string s)
+    {
+        var result = 0;
+        
+        while (s.Split("01").Length - 1 != 0)
+        {
+            s = s.Replace("01", "10");
+            result++;
+        }
+
+        return result;
+    }
+}
