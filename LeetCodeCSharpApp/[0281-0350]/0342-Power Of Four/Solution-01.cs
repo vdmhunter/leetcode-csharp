bb2ef84f72184a8dbb3e71781792b173eadf139e
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-23 01:14:06 +0300

    Add solution #01 for "342. Power of Four"

---
+++ LeetCodeCSharpApp/[0281-0350]/0342-Power Of Four/Solution-01.cs
@@ -0,0 +1,9 @@
+namespace LeetCodeCSharpApp.PowerOfFour01;
+
+public class Solution
+{
+    public bool IsPowerOfFour(int n)
+    {
+        return n == 1 || ( n != 0 && n % 4 == 0 && IsPowerOfFour( n / 4 ) );
+    }
+}
