6ccfb5fd250ed20105c560fc16bc7ccadd645c62
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-08-26 01:38:18 +0300

    Add solution #01 for "383. Ransom Note"

---
+++ LeetCodeCSharpApp/[0351-0420]/0383-Ransom Note/Solution-01.cs
@@ -0,0 +1,15 @@
+namespace LeetCodeCSharpApp.RansomNote01;
+
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var freq = new int[26];
+
+        foreach (var letter in magazine)
+            freq[letter - 'a']++;
+
+        return ransomNote.All(letter => freq[letter - 'a']-- > 0);
+    }
+}
+    
