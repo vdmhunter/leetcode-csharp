c0c93f6dc910b8ec99e5f8d1ade01dde00d0aeef
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-02 02:03:11 +0300

    Add solution #01 for "48. Rotate Image"

---
+++ LeetCodeCSharpApp/[0001-0070]/0048-Rotate Image/Solution-01.cs
@@ -0,0 +1,13 @@
+namespace LeetCodeCSharpApp.RotateImage01;
+
+public class Solution
+{
+    public void Rotate(int[][] matrix)
+    {
+        Array.Reverse(matrix);
+        
+        for (var i = 0; i < matrix.Length; i++)
+            for (var j = 0; j < i; j++)
+                (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+    }
+}
