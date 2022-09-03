9cbe4aea27904f345648a271b36214039b235ee6
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-03 23:00:34 +0300

    Add solution #01 for "2396. Strictly Palindromic Number"

---
+++ LeetCodeCSharpApp/[2381-2450]/2396 - Strictly Palindromic Number/Solution-01.cs
@@ -0,0 +1,34 @@
+namespace LeetCodeCSharpApp.StrictlyPalindromicNumber01;
+
+public class Solution
+{
+    public bool IsStrictlyPalindromic(int n)
+    {
+        for (var i = 2; i <= n - 2; i++)
+        {
+            var arr = DecimalToArbitrarySystem(n, i).ToArray();
+            
+            if (!arr.SequenceEqual(arr.Reverse()))
+                return false;
+        }
+        
+        return true;
+    }
+
+    private static List<int> DecimalToArbitrarySystem(int number, int radix)
+    {
+        if (number == 0)
+            return new List<int> { 0 };
+        
+        var result = new List<int>();
+
+        while (number != 0)
+        {
+            var remainder = number % radix;
+            result.Add(remainder);
+            number /= radix;
+        }
+
+        return result;
+    }
+}
