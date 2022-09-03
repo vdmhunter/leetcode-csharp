0d88461f9a55f9d756a123dfa295e64436095a77
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-03 17:20:09 +0300

    Add solution #02 for "967. Numbers With Same Consecutive Differences"

---
+++ LeetCodeCSharpApp/[0911-0980]/0967-Numbers With Same Consecutive Differences/Solution-02.cs
@@ -0,0 +1,39 @@
+namespace LeetCodeCSharpApp.NumbersWithSameConsecutiveDifferences02;
+
+/// <summary>
+/// DFS
+/// </summary>
+public class Solution
+{
+    // ReSharper disable once IdentifierTypo
+    public int[] NumsSameConsecDiff(int n, int k)
+    {
+        if (n == 1)
+            return new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        var list = new List<int>();
+        
+        for (var i = 1; i <= 9; i++)
+            Dfs(list, n - 1, k, i);
+
+        return list.ToArray();
+    }
+
+    private void Dfs(List<int> list, int n, int k, int num)
+    {
+        if (n <= 0)
+        {
+            list.Add(num);
+            
+            return;
+        }
+
+        var lastDigit = num % 10;
+        
+        if (lastDigit + k <= 9)
+            Dfs(list, n - 1, k, num * 10 + lastDigit + k);
+
+        if (lastDigit - k >= 0 && k != 0)
+            Dfs(list, n - 1, k, num * 10 + lastDigit - k);
+    }
+}
