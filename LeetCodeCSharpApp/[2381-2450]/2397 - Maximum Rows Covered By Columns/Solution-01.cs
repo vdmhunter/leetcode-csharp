e148ef31b45b7eeb2879bddf7d3a53f1b7e7a152
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-03 22:59:22 +0300

    Add solution #01 for "2397. Maximum Rows Covered by Columns"

---
+++ LeetCodeCSharpApp/[2381-2450]/2397 - Maximum Rows Covered By Columns/Solution-01.cs
@@ -0,0 +1,38 @@
+namespace LeetCodeCSharpApp.MaximumRowsCoveredByColumns01;
+
+public class Solution
+{
+    public int MaximumRows(int[][] mat, int cols)
+    {
+        int m = mat.Length, n = mat[0].Length, result = 0;
+        
+        for (var mask = (1 << cols) - 1; mask < 1 << n; mask = NextPopCount(mask))
+        {
+            var rows = 0;
+            
+            for (var i = 0; i < m; ++i)
+                rows += Check(i, mask) == n ? 1 : 0;
+
+            result = Math.Max(result, rows);
+        }
+        
+        int Check(int i, int mask)
+        {
+            int j;
+            
+            for (j = 0; j < n; ++j)
+                if (mat[i][j] == 1 && (mask & (1 << j)) == 0)
+                    break;
+            
+            return j;
+        }
+        
+        return result;
+    }
+
+    private static int NextPopCount(int n)
+    {
+        int c = n & -n, r = n + c;
+        return (((r ^ n) >> 2) / c) | r;
+    }
+}
