80b6dd2f796c14ee0c3a96ae3eec4af48fec035a
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-05 02:42:12 +0300

    Add solution #02 for "2400. Number of Ways to Reach a Position After Exactly k Steps"

---
+++ LeetCodeCSharpApp/[2381-2450]/2400 - Number Of Ways To Reach A Position After Exactly K Steps/Solution-02.cs
@@ -0,0 +1,33 @@
+namespace LeetCodeCSharpApp.NumberOfWaysToReachAPositionAfterExactlyKSteps02;
+
+public class Solution
+{
+    private const int Mode = 1_000_000_007;
+
+    public int NumberOfWays(int startPos, int endPos, int k)
+    {
+        if ((startPos - endPos - k) % 2 != 0)
+            return 0;
+        
+        if (Math.Abs(startPos - endPos) > k)
+            return 0;
+        
+        var result = 1L;
+        
+        for (var i = 0; i < (endPos - startPos + k) / 2; ++i)
+        {
+            result = result * (k - i) % Mode;
+            result = result * Inv(i + 1) % Mode;
+        }
+
+        return (int)result;
+    }
+
+    private static long Inv(long a)
+    {
+        if (a == 1)
+            return 1;
+        
+        return (Mode - Mode / a) * Inv(Mode % a) % Mode;
+    }
+}
