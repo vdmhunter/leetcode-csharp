e1f6d3efee0ebff0b38f9faf1500591e71f5993d
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-05 03:36:36 +0300

    Add solution #01 for "429. N-ary Tree Level Order Traversal"

---
+++ LeetCodeCSharpApp/[0421-0490]/0429 - N-ary Tree Level Order Traversal/Solution-01.cs
@@ -0,0 +1,29 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.NaryTreeLevelOrderTraversal01;
+
+public class Solution
+{
+    public IList<IList<int>> LevelOrder(Node root)
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+        
+        Traverse(root, result);
+        
+        return result;
+    }
+
+    private static void Traverse(Node node, IList<IList<int>> res, int level = 0)
+    {
+        if (node == null)
+            return;
+        
+        if (res.Count <= level)
+            res.Add(new List<int>());
+        
+        res[level].Add(node.val);
+        
+        foreach (var n in node.children)
+            Traverse(n, res, level + 1);
+    }
+}
