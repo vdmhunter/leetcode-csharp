c28d0ed46deec56bb3c9799530b2a7497417a888
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-09-09 11:15:51 +0300

    Add solution #01 for "1996. The Number of Weak Characters in the Game"

---
+++ LeetCodeCSharpApp/[1961-2030]/1996-The Number Of Weak Characters In The Game/Solution-01.cs
@@ -0,0 +1,24 @@
+namespace LeetCodeCSharpApp.TheNumberOfWeakCharactersInTheGame01;
+
+public class Solution
+{
+    public int NumberOfWeakCharacters(int[][] properties)
+    {
+        var n = properties.Length;
+        var count = 0;
+
+        Array.Sort(properties, (a, b) => b[0] == a[0] ? a[1] - b[1] : b[0] - a[0]);
+
+        var max = 0;
+
+        for (var i = 0; i < n; i++)
+        {
+            if (properties[i][1] < max)
+                count++;
+            
+            max = Math.Max(max, properties[i][1]);
+        }
+
+        return count;
+    }
+}
