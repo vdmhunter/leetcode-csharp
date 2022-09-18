fe69c8d383deeb17941dff183276760a6f82fabc
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-19 02:28:17 +0300

    Add solution #01 for "42. Trapping Rain Water"

---
+++ LeetCodeCSharpApp/[0001-0070]/0042-Trapping Rain Water/Solution-01.cs
@@ -0,0 +1,23 @@
+namespace LeetCodeCSharpApp.TrappingRainWater01;
+
+public class Solution
+{
+    public int Trap(int[] height)
+    {
+        int total = 0, high1 = 0, high2 = 0;
+        
+        for (int l = 0, r = height.Length - 1; l < r;)
+            if (height[l] < height[r])
+            {
+                high1 = Math.Max(high1, height[l]);
+                total += high1 - height[l++];
+            }
+            else
+            {
+                high2 = Math.Max(high2, height[r]);
+                total += high2 - height[r--];
+            }
+
+        return total;
+    }
+}
