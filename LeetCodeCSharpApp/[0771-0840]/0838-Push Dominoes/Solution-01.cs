027fdeed2eb951a9a0c213a7a5f30f7b1924d6e3
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-09-28 01:29:26 +0300

    Add solution #01 for "838. Push Dominoes"

---
+++ LeetCodeCSharpApp/[0771-0840]/0838-Push Dominoes/Solution-01.cs
@@ -0,0 +1,49 @@
+using System.Text;
+
+namespace LeetCodeCSharpApp.PushDominoes01;
+
+public class Solution
+{
+    public string PushDominoes(string dominoes)
+    {
+        dominoes = 'L' + dominoes + 'R';
+        var result = new StringBuilder();
+        
+        for (int i = 0, j = 1; j < dominoes.Length; ++j)
+        {
+            if (dominoes[j] == '.')
+                continue;
+            
+            var middle = j - i - 1;
+            
+            if (i > 0)
+                result.Append(dominoes[i]);
+            
+            if (dominoes[i] == dominoes[j])
+            {
+                for (var k = 0; k < middle; k++)
+                    result.Append(dominoes[i]);
+            }
+            else if (dominoes[i] == 'L' && dominoes[j] == 'R')
+            {
+                for (var k = 0; k < middle; k++)
+                    result.Append('.');
+            }
+            else
+            {
+                for (var k = 0; k < middle / 2; k++)
+                    result.Append('R');
+                
+                if (middle % 2 == 1)
+                    result.Append('.');
+                
+                for (var k = 0; k < middle / 2; k++)
+                    result.Append('L');
+            }
+
+            i = j;
+        }
+
+        return result.ToString();
+    }
+}
