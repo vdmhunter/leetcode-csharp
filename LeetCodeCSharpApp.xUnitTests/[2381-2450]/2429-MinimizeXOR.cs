de661c215369ed246463119bbad98dacc9d55b50
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-03 00:43:30 +0300

    Add solution #01 for "2429. Minimize XOR"

---
+++ LeetCodeCSharpApp.xUnitTests/[2381-2450]/2429-MinimizeXOR.cs
@@ -0,0 +1,53 @@
+// ReSharper disable InconsistentNaming
+// ReSharper disable ConvertToConstant.Local
+
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+using Solution01 = LeetCodeCSharpApp.MinimizeXOR01;
+
+namespace LeetCodeCSharpApp.xUnitTests;
+
+[ExcludeFromCodeCoverage]
+public class MinimizeXOR
+{
+    private readonly Solution01.Solution _solution01;
+
+    private const string ProblemName = "2429-Minimize XOR";
+    
+    #region Test Case 001
+
+    private readonly int _testcase001_num1 = 3;
+    private readonly int _testcase001_num2 = 5;
+    private readonly int _testcase001_output = 3;
+
+    #endregion
+
+    #region Test Case 002
+
+    private readonly int _testcase002_num1 = 1;
+    private readonly int _testcase002_num2 = 12;
+    private readonly int _testcase002_output = 3;
+
+    #endregion
+    
+    public MinimizeXOR()
+    {
+        _solution01 = new Solution01.Solution();
+    }
+    
+    [Fact]
+    [Trait("Category", $"{ProblemName}: Solution-01")]
+    public void Solution01_TestCase001()
+    {
+        var output = _solution01.MinimizeXor(_testcase001_num1, _testcase001_num2);
+        Assert.Equal(_testcase001_output, output);
+    }
+
+    [Fact]
+    [Trait("Category", $"{ProblemName}: Solution-01")]
+    public void Solution01_TestCase002()
+    {
+        var output = _solution01.MinimizeXor(_testcase002_num1, _testcase002_num2);
+        Assert.Equal(_testcase002_output, output);
+    }
+}
---
+++ LeetCodeCSharpApp/[2381-2450]/2429-Minimize XOR/Solution-01.cs
@@ -0,0 +1,39 @@
+namespace LeetCodeCSharpApp.MinimizeXOR01;
+
+public class Solution
+{
+    public int MinimizeXor(int num1, int num2)
+    {
+        int a = CountSetBits(num1), b = CountSetBits(num2), result = num1;
+        
+        for (var i = 0; i < 32; ++i)
+        {
+            if (a > b && ((1 << i) & num1) > 0)
+            {
+                result ^= 1 << i;
+                a--;
+            }
+
+            if (a < b && ((1 << i) & num1) == 0)
+            {
+                result ^= 1 << i;
+                a++;
+            }
+        }
+
+        return result;
+    }
+
+    private static int CountSetBits(int n)
+    {
+        var count = 0;
+
+        while (n > 0)
+        {
+            count += n & 1;
+            n >>= 1;
+        }
+
+        return count;
+    }
+}
