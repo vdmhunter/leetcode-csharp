3032822485151adbca29c1a5e25b46ade40ed2d8
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-03 00:44:43 +0300

    Add solution #01 for "2427. Number of Common Factors"

---
+++ LeetCodeCSharpApp/[2381-2450]/2427-Number Of Common Factors/Solution-01.cs
@@ -0,0 +1,28 @@
+namespace LeetCodeCSharpApp.NumberOfCommonFactors01;
+
+public class Solution
+{
+    public int CommonFactors(int a, int b)
+    {
+        var n = Gcd(a, b);
+        var result = 0;
+        
+        for (var i = 1; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i != 0)
+                continue;
+            
+            if (n / i == i)
+                result += 1;
+            else
+                result += 2;
+        }
+ 
+        return result;
+    }
+    
+    private static int Gcd(int a, int b)
+    {
+        return b == 0 ? a : Gcd(b, a % b);
+    }
+}
