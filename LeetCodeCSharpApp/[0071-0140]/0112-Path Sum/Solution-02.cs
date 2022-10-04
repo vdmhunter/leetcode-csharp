3691a169c31a94094e429fc497fd22bd4ac2b84c
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-10-04 18:45:59 +0300

    Add solution #02 for "112. Path Sum"

---
+++ LeetCodeCSharpApp/[0071-0140]/0112-Path Sum/Solution-02.cs
@@ -0,0 +1,38 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.PathSum02;
+
+public class Solution
+{
+    public bool HasPathSum(TreeNode root, int targetSum)
+    {
+        if (root == null!)
+            return false;
+
+        var stack = new Stack<TreeNode>();
+
+        stack.Push(root);
+
+        while (stack.Count != 0)
+        {
+            var pv = stack.Pop();
+
+            if (pv.val == targetSum && pv.left == null! && pv.right == null!)
+                return true;
+
+            if (pv.left != null)
+            {
+                pv.left.val += pv.val;
+                stack.Push(pv.left);
+            }
+
+            if (pv.right != null!)
+            {
+                pv.right.val += pv.val;
+                stack.Push(pv.right);
+            }
+        }
+
+        return false;
+    }
+}
