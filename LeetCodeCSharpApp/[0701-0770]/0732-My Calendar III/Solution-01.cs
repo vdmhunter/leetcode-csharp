1036c6baeb6cc2d1bdc0e19602a5eb12147bfd1d
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-10-07 20:26:47 +0300

    Add solution #01 for "732. My Calendar III"

---
+++ LeetCodeCSharpApp/[0701-0770]/0732-My Calendar III/Solution-01.cs
@@ -0,0 +1,34 @@
+namespace LeetCodeCSharpApp.MyCalendarIII01;
+
+// Your MyCalendarThree object will be instantiated and called as such:
+// MyCalendarThree obj = new MyCalendarThree();
+// int param_1 = obj.Book(start,end);
+
+/// <summary>
+/// Sweep-line Algorithm
+/// </summary>
+public class MyCalendarThree
+{
+    private readonly SortedDictionary<int, int> _diff;
+
+    public MyCalendarThree()
+    {
+        _diff = new SortedDictionary<int, int>();
+    }
+
+    public int Book(int start, int end)
+    {
+        _diff[start] = _diff.GetValueOrDefault(start, 0) + 1;
+        _diff[end] = _diff.GetValueOrDefault(end, 0) - 1;
+        
+        int result = 0, cur = 0;
+
+        foreach (var delta in _diff.Values)
+        {
+            cur += delta;
+            result = Math.Max(result, cur);
+        }
+
+        return result;
+    }
+}
