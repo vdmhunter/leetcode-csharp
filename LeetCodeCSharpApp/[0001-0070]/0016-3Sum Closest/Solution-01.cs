cf04ed48798ed76ffbf9e4def1b919bf3347d5fe
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-08 18:40:56 +0300

    Add solution #01 for "16. 3Sum Closest"

---
+++ LeetCodeCSharpApp/[0001-0070]/0016-3Sum Closest/Solution-01.cs
@@ -0,0 +1,40 @@
+namespace LeetCodeCSharpApp.ThreeSumClosest01;
+
+public class Solution
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        var n = nums.Length;
+        var result = nums[0] + nums[1] + nums[2];
+        
+        if(result > target)
+            return result;
+
+        for (var i = 0; i < n - 2; i++)
+        {
+            var low = i + 1;
+            var high = n - 1;
+
+            while (low < high)
+            {
+                result = nums[i] + nums[low] + nums[high];
+
+                if (result == target)
+                    return target;
+
+                MovePointers(ref low, ref high);
+            }
+        }
+        
+        void MovePointers(ref int l, ref int h)
+        {
+            if (result < target)
+                l++;
+            else
+                h--;
+        }
+
+        return result;
+    }
+}
