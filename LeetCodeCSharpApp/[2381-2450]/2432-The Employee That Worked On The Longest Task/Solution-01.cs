4ffe567cef08f3fd787086e6dfa95b7fb6601888
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-09 16:29:14 +0300

    Add solution #01 for "2432. The Employee That Worked on the Longest Task"

---
+++ LeetCodeCSharpApp/[2381-2450]/2432-The Employee That Worked On The Longest Task/Solution-01.cs
@@ -0,0 +1,15 @@
+namespace LeetCodeCSharpApp.TheEmployeeThatWorkedOnTheLongestTask01;
+
+public class Solution
+{
+    public int HardestWorker(int n, int[][] logs)
+    {
+        var taskDurations = logs.Skip(1).Zip(logs, (curr, prev) => (Id:curr[0], Duration:curr[1] - prev[1])).ToList();
+        taskDurations.Insert(0, (logs[0][0], logs[0][1]));
+        
+        var maxDuration = taskDurations.MaxBy(d => d.Duration).Duration;
+        var result = taskDurations.Where(d => d.Duration == maxDuration).MinBy(d => d.Id).Id;
+
+        return result;
+    }
+}
