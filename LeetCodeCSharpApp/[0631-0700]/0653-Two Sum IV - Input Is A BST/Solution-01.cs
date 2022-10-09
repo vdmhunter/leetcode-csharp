d2be7c6e8927bc92cdb39017588fcbc6c08e4841
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-09 20:38:23 +0300

    Add solution #01 for "653. Two Sum IV - Input is a BST"

---
+++ LeetCodeCSharpApp/[0631-0700]/0653-Two Sum IV - Input Is A BST/Solution-01.cs
@@ -0,0 +1,21 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.TwoSumIVInputIsABst01;
+
+public class Solution
+{
+    private readonly HashSet<int> _values = new();
+    
+    public bool FindTarget(TreeNode root, int k)
+    {
+        if (root == null)
+            return false;
+
+        if (_values.Contains(k - root.val))
+            return true;
+        
+        _values.Add(root.val);
+
+        return FindTarget(root.left, k) || FindTarget(root.right, k);
+    }
+}
