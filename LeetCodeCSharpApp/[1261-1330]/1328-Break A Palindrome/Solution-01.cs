fa6bcb86e5f1fee7c2c263cc0b6ca3810b390177
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-10-10 13:01:49 +0300

    Add solution #01 for "1328. Break a Palindrome"

---
+++ LeetCodeCSharpApp/[1261-1330]/1328-Break A Palindrome/Solution-01.cs
@@ -0,0 +1,22 @@
+namespace LeetCodeCSharpApp.BreakAPalindrome01;
+
+public class Solution
+{
+    public string BreakPalindrome(string palindrome)
+    {
+        var s = palindrome.ToCharArray();
+        var n = s.Length;
+
+        for (var i = 0; i < n / 2; i++)
+            if (s[i] != 'a')
+            {
+                s[i] = 'a';
+                
+                return new string(s);
+            }
+
+        s[n - 1] = 'b'; //if all 'a'
+
+        return n < 2 ? "" : new string(s);
+    }
+}
