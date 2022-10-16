08be49084cc8a94ff01dde4ed0af80d2390771d2
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-17 01:19:34 +0300

    Add solution #01 for "2442. Count Number of Distinct Integers After Reverse Operations"

---
+++ LeetCodeCSharpApp/[2381-2450]/2442-Count Number Of Distinct Integers After Reverse Operations/Solution-01.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeCSharpApp.CountNumberOfDistinctIntegersAfterReverseOperations01;
+
+public class Solution
+{
+    public int CountDistinctIntegers(int[] nums)
+    {
+        var list = nums.ToList();
+        list.AddRange(nums.Select(Reverse));
+
+        return list.Distinct().Count();
+    }
+
+    private static int Reverse(int n)
+    {
+        var result = 0;
+        
+        while (n > 0)
+        {
+            var remainder = n % 10;
+            result = result * 10 + remainder;
+            n /= 10;
+        }
+
+        return result;
+    }
+}
