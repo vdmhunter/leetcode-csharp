6c896a5f38fa2d348858f2c871e37f3389cdcd74
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-18 21:41:51 +0300

    Add solution #01 for "38. Count and Say"

---
+++ LeetCodeCSharpApp/[0001-0070]/0038-Count And Say/Solution-01.cs
@@ -0,0 +1,39 @@
+using System.Text;
+
+namespace LeetCodeCSharpApp.CountAndSay01;
+
+public class Solution
+{
+    public string CountAndSay(int n)
+    {
+        var s = "1";
+        
+        for (var i = 1; i < n; i++)
+            s = CountIdx(s);
+        
+        return s;
+    }
+
+    private string CountIdx(string s)
+    {
+        var sb = new StringBuilder();
+        var c = s[0];
+        var count = 1;
+        
+        for (var i = 1; i < s.Length; i++)
+            if (s[i] == c)
+                count++;
+            else
+            {
+                sb.Append(count);
+                sb.Append(c);
+                c = s[i];
+                count = 1;
+            }
+
+        sb.Append(count);
+        sb.Append(c);
+        
+        return sb.ToString();
+    }
+}
