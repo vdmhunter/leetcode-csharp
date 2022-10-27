37d5eadf2f5c8c5123cb8ffaff5207529a402f84
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-10-27 12:21:13 +0300

    Add solution #01 for "523. Continuous Subarray Sum"

---
+++ LeetCodeCSharpApp/[0491-0560]/0523-Continuous Subarray Sum/Solution-01.cs
@@ -0,0 +1,28 @@
+namespace LeetCodeCSharpApp.ContinuousSubarraySum01;
+
+public class Solution
+{
+    public bool CheckSubarraySum(int[] nums, int k)
+    {
+        var dictionary = new Dictionary<int, int> { { 0, -1 } };
+        var runningSum = 0;
+        
+        for (var i = 0; i < nums.Length; i++)
+        {
+            runningSum += nums[i];
+            
+            if (k != 0)
+                runningSum %= k;
+
+            if (dictionary.ContainsKey(runningSum))
+            {
+                if (i - dictionary[runningSum] > 1)
+                    return true;
+            }
+            else
+                dictionary.Add(runningSum, i);
+        }
+
+        return false;
+    }
+}
