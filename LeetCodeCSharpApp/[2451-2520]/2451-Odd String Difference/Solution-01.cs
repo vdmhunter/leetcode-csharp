857075f42451c23bdde2052701d85cc60bb8c7e5
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-10-29 21:51:28 +0300

    Add solution #01 for "2451. Odd String Difference"

---
+++ LeetCodeCSharpApp/[2451-2520]/2451-Odd String Difference/Solution-01.cs
@@ -0,0 +1,26 @@
+namespace LeetCodeCSharpApp.OddStringDifference01;
+
+public class Solution
+{
+    public string OddString(string[] words)
+    {
+        var counts = new Dictionary<string, (int Count, string Word)>(2);
+
+        foreach (var word in words)
+        {
+            var arr = GetDifferenceArrayAsString(word);
+
+            if (counts.ContainsKey(arr))
+                counts[arr] = (counts[arr].Count + 1, counts[arr].Word);
+            else
+                counts.Add(arr, (1, word));
+        }
+
+        return counts.MinBy(d => d.Value.Count).Value.Word;
+    }
+
+    private string GetDifferenceArrayAsString(string word)
+    {
+        return string.Join(",", word.Skip(1).Zip(word, (curr, prev) => curr - prev));
+    } 
+}
