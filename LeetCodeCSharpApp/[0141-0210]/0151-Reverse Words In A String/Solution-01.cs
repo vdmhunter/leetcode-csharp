6f80660d1d9fc8ee0d0f39807ef64f51e4cd6cae
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-11-13 19:41:50 +0300

    Add solution #01 for "151. Reverse Words in a String"

---
+++ LeetCodeCSharpApp/[0141-0210]/0151-Reverse Words In A String/Solution-01.cs
@@ -0,0 +1,13 @@
+namespace LeetCodeCSharpApp.ReverseWordsInAString01;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        var words = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        
+        Array.Reverse(words);
+        
+        return string.Join(" ", words);
+    }
+}
