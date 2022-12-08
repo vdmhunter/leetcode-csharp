8201ff6fee47b1bc977acca93401345bf6e6057a
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-12-09 02:09:03 +0300

    Add solution #02 for "872. Leaf-Similar Trees"

---
+++ LeetCodeCSharpApp/[0841-0910]/0872-Leaf-Similar Trees/Solution-02.cs
@@ -0,0 +1,36 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.LeafSimilarTrees02;
+
+public class Solution
+{
+    private string _s1 = "";
+
+    public bool LeafSimilar(TreeNode root1, TreeNode root2)
+    {
+        Helper(root1);
+
+        var temp = _s1;
+        _s1 = "";
+
+        Helper(root2);
+
+        return _s1 == temp;
+    }
+
+    private void Helper(TreeNode n)
+    {
+        if (n.left == null && n.right == null)
+        {
+            _s1 = _s1 + ":" + n.val;
+        }
+        else
+        {
+            if (n.left != null) 
+                Helper(n.left);
+
+            if (n.right != null) 
+                Helper(n.right);
+        }
+    }
+}
