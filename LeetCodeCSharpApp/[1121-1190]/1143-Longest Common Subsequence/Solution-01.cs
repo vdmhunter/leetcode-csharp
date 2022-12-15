ad39c7c20ba5711a6a001cc8557b0d0528f369bd
Vladimir Dmitriev <dmitriev@ofd.ru>
2022-12-15 20:10:38 +0300

    Add solution #01 for "1143. Longest Common Subsequence"

---
+++ LeetCodeCSharpApp/[1121-1190]/1143-Longest Common Subsequence/Solution-01.cs
@@ -0,0 +1,20 @@
+namespace LeetCodeCSharpApp.LongestCommonSubsequence01;
+
+public class Solution
+{
+    public int LongestCommonSubsequence(string text1, string text2)
+    {
+        var dp = new int[text1.Length + 1, text2.Length + 1];
+
+        for (var i = 1; i <= text1.Length; i++)
+        {
+            for (var j = 1; j <= text2.Length; j++)
+                if (text1[i - 1] == text2[j - 1])
+                    dp[i, j] = 1 + dp[i - 1, j - 1];
+                else
+                    dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+        }
+        
+        return dp[text1.Length, text2.Length];
+    }
+}
