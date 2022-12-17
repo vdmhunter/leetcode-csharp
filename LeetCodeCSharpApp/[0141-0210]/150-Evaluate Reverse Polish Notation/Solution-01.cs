f5570d182493ab9c806a0a88c22a6f5a0754d5da
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-12-18 02:10:11 +0300

    Add solution #01 for "150. Evaluate Reverse Polish Notation"

---
+++ LeetCodeCSharpApp/[0141-0210]/150-Evaluate Reverse Polish Notation/Solution-01.cs
@@ -0,0 +1,35 @@
+// ReSharper disable InconsistentNaming
+
+namespace LeetCodeCSharpApp.EvaluateReversePolishNotation01;
+
+public class Solution
+{
+    public int EvalRPN(string[] tokens)
+    {
+        var stack = new Stack<int>();
+
+        foreach (var token in tokens)
+            if (token is "+" or "-" or "*" or "/")
+            {
+                var right = stack.Pop();
+                var left = stack.Pop();
+
+                var result = token switch
+                {
+                    "+" => left + right,
+                    "-" => left - right,
+                    "*" => left * right,
+                    "/" => left / right,
+                    _ => throw new ArgumentOutOfRangeException(nameof(token))
+                };
+
+                stack.Push(result);
+            }
+            else
+            {
+                stack.Push(int.Parse(token));
+            }
+
+        return stack.Pop();
+    }
+}
