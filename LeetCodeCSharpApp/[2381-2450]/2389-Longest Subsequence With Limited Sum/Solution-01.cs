10cc1982e9dfd929e952910303142093a64d3f43
Vladimir Dmitriev <vdmhunter@gmail.com>
2022-12-26 01:56:19 +0300

    Add solution #01 for "2389. Longest Subsequence With Limited Sum"

---
+++ LeetCodeCSharpApp/[2381-2450]/2389-Longest Subsequence With Limited Sum/Solution-01.cs
@@ -0,0 +1,22 @@
+namespace LeetCodeCSharpApp.LongestSubsequenceWithLimitedSum01;
+
+public class Solution
+{
+    public int[] AnswerQueries(int[] nums, int[] queries)
+    {
+        Array.Sort(nums);
+        int n = nums.Length, m = queries.Length;
+        var result = new int[m];
+        
+        for (var i = 1; i < n; ++i)
+            nums[i] += nums[i - 1];
+        
+        for (var i = 0; i < m; ++i)
+        {
+            var j = Array.BinarySearch(nums, queries[i]);
+            result[i] = Math.Abs(j + 1);
+        }
+
+        return result;
+    }
+}
