df893f2197ec7c98f2718bdd12ebfd53354328e5
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-04 13:46:32 +0300

    Add solution #01 for "944. Delete Columns to Make Sorted"

---
+++ LeetCodeCSharpApp/[0911-0980]/0944-Delete Columns To Make Sorted/Solution-01.cs
@@ -0,0 +1,20 @@
+// ReSharper disable IdentifierTypo
+namespace LeetCodeCSharpApp.DeleteColumnsToMakeSorted01;
+
+public class Solution
+{
+    public int MinDeletionSize(string[] strs)
+    {
+        var result = strs[0].Length;
+
+        for (var i = 0; i < strs[0].Length; i++)
+        {
+            var col = strs.Select(s => s[i]).ToArray();
+            
+            if (col.OrderBy(c => c).SequenceEqual(col))
+                result--;
+        }
+
+        return result;
+    }
+}
