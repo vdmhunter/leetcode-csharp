63dec8e895be2f21ed0a91f04b4e7a96622c86a4
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-08 18:50:41 +0300

    Add solution #01 for "134. Gas Station"

---
+++ LeetCodeCSharpApp/[0071-0140]/0134-Gas Station/Solution-01.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeCSharpApp.GasStation01;
+
+public class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        var sum = gas[^1] - cost[^1];
+        var maxIndex = gas.Length - 1;
+        var maxSum = sum;
+
+        for (var i = gas.Length - 2; i >= 0; i--)
+        {
+            sum += gas[i] - cost[i];
+
+            if (sum <= maxSum)
+                continue;
+            
+            maxIndex = i;
+            maxSum = sum;
+        }
+
+        if (sum < 0)
+            return -1;
+        
+        return maxIndex;
+    }
+}
