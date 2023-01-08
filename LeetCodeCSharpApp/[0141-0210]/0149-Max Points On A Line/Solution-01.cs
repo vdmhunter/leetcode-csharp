2635584c4cd8591037b777fc50e62c8e49922055
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-08 19:05:08 +0300

    Add solution #01 for "149. Max Points on a Line"

---
+++ LeetCodeCSharpApp/[0141-0210]/0149-Max Points On A Line/Solution-01.cs
@@ -0,0 +1,41 @@
+namespace LeetCodeCSharpApp.MaxPointsOnALine01;
+
+public class Solution
+{
+    public int MaxPoints(int[][] points)
+    {
+        var result = 0;
+        
+        for (var i = 0; i < points.Length; i++)
+        {
+            var dic = new Dictionary<double, int>();
+            var max = 0;
+            
+            for (var j = i + 1; j < points.Length; j++)
+            {
+                var slope = GetSlope(points[i], points[j]);
+                
+                if (!dic.ContainsKey(slope))
+                    dic[slope] = 0;
+
+                dic[slope]++;
+                max = Math.Max(max, dic[slope]);
+            }
+
+            result = Math.Max(result, max + 1);
+        }
+
+        return result;
+    }
+
+    private double GetSlope(int[] point1, int[] point2)
+    {
+        double dx = point2[0] - point1[0];
+        double dy = point2[1] - point1[1];
+
+        if (dx == 0)
+            return double.MaxValue;
+
+        return dy / dx;
+    }
+}
