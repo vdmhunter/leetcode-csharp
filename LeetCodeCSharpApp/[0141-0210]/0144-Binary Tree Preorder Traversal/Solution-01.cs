64187597e178b6371dd91145f347af225629494d
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-09 22:03:47 +0300

    Add solution #01 for "144. Binary Tree Preorder Traversal"

---
+++ LeetCodeCSharpApp/[0141-0210]/0144-Binary Tree Preorder Traversal/Solution-01.cs
@@ -0,0 +1,26 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.BinaryTreePreorderTraversal01;
+
+public class Solution
+{
+    private readonly List<int> _result = new();
+
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        Dfs(root);
+        
+        return _result;
+    }
+
+    private void Dfs(TreeNode node)
+    {
+        if (node == null)
+            return;
+
+        _result.Add(node.val);
+
+        Dfs(node.left);
+        Dfs(node.right);
+    }
+}
