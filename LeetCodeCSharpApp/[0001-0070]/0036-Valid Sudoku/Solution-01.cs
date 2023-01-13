21da710daf636a2382f416c741d435dcfffd62fc
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-13 03:10:02 +0300

    Add solution #01 for "36. Valid Sudoku"

---
+++ LeetCodeCSharpApp/[0001-0070]/0036-Valid Sudoku/Solution-01.cs
@@ -0,0 +1,25 @@
+namespace LeetCodeCSharpApp.ValidSudoku01;
+
+public class Solution
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        var seen = new HashSet<string>();
+
+        for (var i = 0; i < 9; ++i)
+            for (var j = 0; j < 9; ++j)
+            {
+                var number = board[i][j];
+
+                if (number == '.')
+                    continue;
+
+                if (!seen.Add(number + " in row " + i) ||
+                    !seen.Add(number + " in column " + j) ||
+                    !seen.Add(number + " in block " + i / 3 + "-" + j / 3))
+                    return false;
+            }
+
+            return true;
+    }
+}
