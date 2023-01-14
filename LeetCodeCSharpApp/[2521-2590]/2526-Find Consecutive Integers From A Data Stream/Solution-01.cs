fe1f0654e847bc074dde8187b1944dda40fbe8b0
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-14 22:03:28 +0300

    Add solution #01 for "2526. Find Consecutive Integers from a Data Stream"

---
+++ LeetCodeCSharpApp/[2521-2590]/2526-Find Consecutive Integers From A Data Stream/Solution-01.cs
@@ -0,0 +1,34 @@
+// ReSharper disable IdentifierTypo
+
+namespace LeetCodeCSharpApp.FindConsecutiveIntegersFromADataStream01;
+
+public class DataStream
+{
+    private int _count;
+    private int _countOfValue;
+    private readonly int _value;
+    private readonly int _k;
+    
+    public DataStream(int value, int k)
+    {
+        _count = 0;
+        _countOfValue = 0;
+        _value = value;
+        _k = k;
+    }
+    
+    public bool Consec(int num)
+    {
+        _count++;
+        
+        if (num == _value)
+            _countOfValue++;
+        else
+            _countOfValue = 0;
+        
+        if (_count < _k)
+             return false;
+
+        return _countOfValue >= _k;
+    }
+}
