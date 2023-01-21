5d9fa771915d20b475613428598908656bba4daf
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-22 01:06:40 +0300

    Small refactoring for "834. Sum of Distances in Tree" (solution #01)

--- LeetCodeCSharpApp/[0771-0840]/0834-Sum Of Distances In Tree/Solution-01.cs
+++ LeetCodeCSharpApp/[0771-0840]/0834-Sum Of Distances In Tree/Solution-01.cs
@@ -4,10 +4,10 @@ public class Solution
 {
     public int[] SumOfDistancesInTree(int n, int[][] edges)
     {
+        var result = new int[n];
         var graph = new List<int>[n];
         var count = new int[n];
         Array.Fill(count, 1);
-        var answer = new int[n];
 
         for (var i = 0; i < graph.Length; i++)
             graph[i] = new List<int>();
@@ -18,28 +18,28 @@ public class Solution
             graph[edge[1]].Add(edge[0]);
         }
 
-        PostOrder(0, -1, graph, count, answer);
-        PreOrder(0, -1, graph, count, answer, n);
+        PostOrder(0, -1, graph, count, result);
+        PreOrder(0, -1, graph, count, result, n);
 
-        return answer;
+        return result;
     }
 
-    private static void PostOrder(int node, int parent, List<int>[] graph, int[] count, int[] answer)
+    private static void PostOrder(int node, int parent, List<int>[] graph, int[] count, int[] result)
     {
         foreach (var child in graph[node].Where(child => child != parent))
         {
-            PostOrder(child, node, graph, count, answer);
+            PostOrder(child, node, graph, count, result);
             count[node] += count[child];
-            answer[node] += answer[child] + count[child];
+            result[node] += result[child] + count[child];
         }
     }
 
-    private static void PreOrder(int node, int parent, List<int>[] graph, int[] count, int[] answer, int n)
+    private static void PreOrder(int node, int parent, List<int>[] graph, int[] count, int[] result, int n)
     {
         foreach (var child in graph[node].Where(child => child != parent))
         {
-            answer[child] = answer[node] + (n - count[child]) - count[child];
-            PreOrder(child, node, graph, count, answer, n);
+            result[child] = result[node] + (n - count[child]) - count[child];
+            PreOrder(child, node, graph, count, result, n);
         }
     }
 }
