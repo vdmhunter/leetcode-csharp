3fce3d4d0b96da61280b12ed62eb4a91e528100b
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-22 01:05:49 +0300

    Small refactoring for "1657. Determine if Two Strings Are Close" (solution #01)

--- LeetCodeCSharpApp/[1611-1680]/1657-Determine If Two Strings Are Close/Solution-01.cs
+++ LeetCodeCSharpApp/[1611-1680]/1657-Determine If Two Strings Are Close/Solution-01.cs
@@ -5,12 +5,15 @@ public class Solution
     public bool CloseStrings(string word1, string word2)
     {
         var hs = new HashSet<char>(word2);
-        int[] freq1 = new int[26], freq2 = new int[26];
+        var freq1 = new int[26];
+        var freq2 = new int[26];
 
         foreach (var c in word1)
         {
             freq1[c - 'a']++;
-            if (hs.Add(c)) return false;
+
+            if (hs.Add(c))
+                return false;
         }
 
         foreach (var c in word2)
