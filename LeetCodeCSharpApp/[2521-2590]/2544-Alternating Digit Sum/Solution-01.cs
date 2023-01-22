c06bf3b4753617124f184d5ef90562ba14bd1bbe
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-22 20:19:22 +0300

    Add solution #01 for "2544. Alternating Digit Sum"

---
+++ LeetCodeCSharpApp/[2521-2590]/2544-Alternating Digit Sum/Solution-01.cs
@@ -0,0 +1,18 @@
+namespace LeetCodeCSharpAppAlternatingDigitSum01;
+
+public class Solution
+{
+    public int AlternateDigitSum(int n)
+    {
+        var nString = n.ToString();
+        var totalSum = int.Parse(nString[0].ToString());
+        
+        for (var i = 1; i < nString.Length; i++)
+            if (i % 2 == 0)
+                totalSum += int.Parse(nString[i].ToString());
+            else
+                totalSum -= int.Parse(nString[i].ToString());
+        
+        return totalSum;
+    }
+}
