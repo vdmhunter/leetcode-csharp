95131a18a77cd205dae1e5d5ca5548254acbf18c
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-01-29 22:05:46 +0300

    Small refactoring for "62. Unique Paths" (solution #02)

--- LeetCodeCSharpApp/[0001-0070]/0062-Unique Paths/Solution-02.cs
+++ LeetCodeCSharpApp/[0001-0070]/0062-Unique Paths/Solution-02.cs
@@ -6,8 +6,8 @@ public class Solution
 {
     public int UniquePaths(int m, int n)
     {
-        BigInteger Factorial(int x) => x > 1 ? BigInteger.Multiply(x, Factorial(x - 1)) : 1;
+        BigInteger Factorial(int x) => x > 1 ? x * Factorial(x - 1) : 1;
 
-        return (int)BigInteger.Divide(BigInteger.Divide(Factorial(m + n - 2), Factorial(m - 1)), Factorial(n - 1));
+        return (int)(Factorial(m + n - 2) / Factorial(m - 1) / Factorial(n - 1));
     }
 }
