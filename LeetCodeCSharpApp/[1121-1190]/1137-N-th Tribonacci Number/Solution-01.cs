eb644fe8c24070e4029649742b10fb95f4e28eab
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-02-01 04:07:42 +0300

    Small refactoring for "1137. N-th Tribonacci Number" (solution #01)

--- LeetCodeCSharpApp/[1121-1190]/1137-N-th Tribonacci Number/Solution-01.cs
+++ LeetCodeCSharpApp/[1121-1190]/1137-N-th Tribonacci Number/Solution-01.cs
@@ -6,7 +6,7 @@ public class Solution
 {
     public int Tribonacci(int n)
     {
-        var t = new int[] { 0, 1, 1 };
+        var t = new[] { 0, 1, 1 };
 
         for (var i = 3; i <= n; ++i)
             t[i % 3] = t[0] + t[1] + t[2];
