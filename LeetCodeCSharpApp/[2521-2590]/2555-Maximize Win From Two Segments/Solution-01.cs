e741efe8f72fd0bbd5be0538ad5082f75ad9df01
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-02-04 19:37:35 +0300

    Add solution #01 for "2555. Maximize Win From Two Segments"

---
+++ LeetCodeCSharpApp/[2521-2590]/2555-Maximize Win From Two Segments/Solution-01.cs
@@ -0,0 +1,21 @@
+namespace LeetCodeCSharpApp.MaximizeWinFromTwoSegments;
+
+public class Solution
+{
+    public int MaximizeWin(int[] prizePositions, int k)
+    {
+        int result = 0, n = prizePositions.Length, j = 0;
+        var dp = new int[n + 1];
+
+        for (var i = 0; i < n; ++i)
+        {
+            while (prizePositions[j] < prizePositions[i] - k)
+                ++j;
+            
+            dp[i + 1] = Math.Max(dp[i], i - j + 1);
+            result = Math.Max(result, i - j + 1 + dp[j]);
+        }
+
+        return result;
+    }
+}
