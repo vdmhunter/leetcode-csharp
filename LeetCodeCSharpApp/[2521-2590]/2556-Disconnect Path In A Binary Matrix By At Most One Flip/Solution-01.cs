41a2362908aa6a5b5002416a7e73b78aa0c00d50
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-02-04 20:14:10 +0300

    Add solution #01 for "2556. Disconnect Path in a Binary Matrix by at Most One Flip"

---
+++ LeetCodeCSharpApp/[2521-2590]/2556-Disconnect Path In A Binary Matrix By At Most One Flip/Solution-01.cs
@@ -0,0 +1,41 @@
+namespace LeetCodeCSharpApp.DisconnectPathInABinaryMatrixByAtMostOneFlip01;
+
+public class Solution
+{
+    private static readonly int[][] Dirs = { new[] { 0, 1 }, new[] { 1, 0 } };
+
+    public bool IsPossibleToCutPath(int[][] grid)
+    {
+        Dfs(grid, 0, 0);
+        var second = Dfs(grid, 0, 0);
+        
+        return second < 1;
+    }
+
+    private static int Dfs(int[][] g, int r, int c)
+    {
+        if (r < 0 || r >= g.Length || c < 0 || c >= g[0].Length || g[r][c] == 0)
+            return 0;
+        
+        if (r == g.Length - 1 && c == g[0].Length - 1)
+            return 1;
+
+        if (!(r == 0 && c == 0))
+            g[r][c] = 0;
+
+        return Count(g, r, c, 0);
+    }
+
+    private static int Count(int[][] g, int r, int c, int count)
+    {
+        foreach (var dir in Dirs)
+        {
+            count += Dfs(g, r + dir[0], c + dir[1]);
+
+            if (count >= 1)
+                break;
+        }
+
+        return count;
+    }
+}
