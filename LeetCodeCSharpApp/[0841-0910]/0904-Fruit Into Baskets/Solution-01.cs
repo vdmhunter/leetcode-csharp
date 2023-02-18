b30227140d3abb06fb4517aa7f759281145a8866
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-02-19 00:02:27 +0300

    Add solution #01 for "904. Fruit Into Baskets"

---
+++ LeetCodeCSharpApp/[0841-0910]/0904-Fruit Into Baskets/Solution-01.cs
@@ -0,0 +1,25 @@
+namespace LeetCodeCSharpApp.FruitIntoBaskets01;
+
+public class Solution
+{
+    public int TotalFruit(int[] fruits)
+    {
+        int result = 0, current = 0, countB = 0, a = 0, b = 0;
+        
+        foreach (var c in fruits)
+        {
+            current = c == a || c == b ? current + 1 : countB + 1;
+            countB = c == b ? countB + 1 : 1;
+            
+            if (b != c)
+            {
+                a = b;
+                b = c;
+            }
+
+            result = Math.Max(result, current);
+        }
+
+        return result;
+    }
+}
