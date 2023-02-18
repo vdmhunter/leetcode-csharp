f4253db0cfeb3d4aae6e73f98a4d0e4119139519
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-02-18 23:56:39 +0300

    Add solution #01 for "2567. Minimum Score by Changing Two Elements"

---
+++ LeetCodeCSharpApp/[2521-2590]/2567-Minimum Score By Changing Two Elements/Solution-01.cs
@@ -0,0 +1,12 @@
+namespace LeetCodeCSharpApp.MinimumScoreByChangingTwoElements01;
+
+public class Solution
+{
+    public int MinimizeSum(int[] nums)
+    {
+        var n = nums.Length;
+        Array.Sort(nums);
+
+        return new[] { nums[n - 1] - nums[2], nums[n - 3] - nums[0], nums[n - 2] - nums[1] }.Min();
+    }
+}
