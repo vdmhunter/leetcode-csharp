18e7791bdb2b933feddff2a33043cda9c8b6c9ae
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-02-19 21:02:37 +0300

    Add solution #01 for "103. Binary Tree Zigzag Level Order Traversal"

---
+++ LeetCodeCSharpApp/[0071-0140]/0103-Binary Tree Zigzag Level Order Traversal/Solution-01.cs
@@ -0,0 +1,36 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.BinaryTreeZigzagLevelOrderTraversal01;
+
+public class Solution
+{
+    private readonly Dictionary<int, List<int>> _dict = new();
+
+    public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+    {
+        var ls = new List<IList<int>>();
+        Dfs(root, 0);
+        var bol = true;
+
+        foreach (var item in _dict.Values)
+        {
+            ls.Add(bol ? item : item.Select(x => x).Reverse().ToList());
+            bol = !bol;
+        }
+
+        return ls;
+    }
+
+    private void Dfs(TreeNode root, int i)
+    {
+        if (root == null)
+            return;
+
+        if (!_dict.ContainsKey(i))
+            _dict.Add(i, new List<int>());
+
+        _dict[i].Add(root.val);
+        Dfs(root.left, i + 1);
+        Dfs(root.right, i + 1);
+    }
+}
