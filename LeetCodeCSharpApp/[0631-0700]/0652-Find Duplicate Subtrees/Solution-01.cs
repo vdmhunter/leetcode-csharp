5c77a6731b2304250272417e6da0d63e6a2b461a
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-03-01 23:12:05 +0300

    Add solution #01 for "652. Find Duplicate Subtrees"

---
+++ LeetCodeCSharpApp/[0631-0700]/0652-Find Duplicate Subtrees/Solution-01.cs
@@ -0,0 +1,41 @@
+using LeetCodeCSharpApp.Common;
+
+namespace LeetCodeCSharpApp.FindDuplicateSubtrees01;
+
+public class Solution
+{
+    public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+    {
+        var result = new List<TreeNode>();
+        var tripletToId = new Dictionary<string, int>();
+        var counts = new Dictionary<int, int>();
+        Traverse(root, tripletToId, counts, result);
+
+        return result;
+    }
+
+    private static int Traverse(TreeNode node, Dictionary<string, int> tripletToId, Dictionary<int, int> counts,
+        List<TreeNode> result)
+    {
+        if (node == null)
+            return 0;
+
+        var triplet = string.Format("{0},{1},{2}", Traverse(node.left, tripletToId, counts, result), node.val,
+            Traverse(node.right, tripletToId, counts, result));
+
+        if (!tripletToId.ContainsKey(triplet))
+            tripletToId.Add(triplet, tripletToId.Count + 1);
+
+        var id = tripletToId[triplet];
+
+        if (counts.ContainsKey(id))
+            counts[id]++;
+        else
+            counts.Add(id, 1);
+
+        if (counts[id] == 2)
+            result.Add(node);
+
+        return id;
+    }
+}
