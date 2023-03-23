7b8fa2f7deb2e6745bd3d5d9df962c76fe0501f5
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-03-23 23:58:17 +0300

    Add solution #01 for "1319. Number of Operations to Make Network Connected"

---
+++ LeetCodeCSharpApp/[1261-1330]/1319-Number Of Operations To Make Network Connected/Solution-01.cs
@@ -0,0 +1,54 @@
+namespace LeetCodeCSharpApp.NumberOfOperationsToMakeNetworkConnected01;
+
+public class Solution
+{
+    public int MakeConnected(int n, int[][] connections)
+    {
+        var parent = new int[n];
+        var ranks = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            ranks[i] = 1;
+        }
+
+        var spareConnections = 0;
+
+        foreach (var connection in connections)
+        {
+            var v1 = connection[0];
+            var v2 = connection[1];
+            var r1 = GetRoot(parent, v1);
+            var r2 = GetRoot(parent, v2);
+
+            if (r1 == r2)
+                spareConnections++;
+            else
+            {
+                if (ranks[r1] < ranks[r2]) (r1, r2) = (r2, r1);
+
+                parent[r2] = r1;
+                ranks[r1] += ranks[r2];
+            }
+        }
+
+        var components = new HashSet<int>();
+        
+        for (var i = 0; i < n; i++)
+            components.Add(GetRoot(parent, i));
+
+        return components.Count - 1 > spareConnections ? -1 : components.Count - 1;
+    }
+
+    private static int GetRoot(int[] parent, int v)
+    {
+        while (v != parent[v])
+        {
+            parent[v] = parent[parent[v]];
+            v = parent[v];
+        }
+
+        return v;
+    }
+}
