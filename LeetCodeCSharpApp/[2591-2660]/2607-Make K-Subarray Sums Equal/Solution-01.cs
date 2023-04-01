10dfd845f444191714208f1463c39b370d126465
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-04-01 22:33:33 +0300

    Add solution #01 for "2607. Make K-Subarray Sums Equal"

---
+++ LeetCodeCSharpApp/[2591-2660]/2607-Make K-Subarray Sums Equal/Solution-01.cs
@@ -0,0 +1,38 @@
+namespace LeetCodeCSharpApp.MakeKSubarraySumsEqual01;
+
+public class Solution
+{
+    public long MakeSubKSumEqual(int[] arr, int k)
+    {
+        var result = 0L;
+        var shift = GetMin(arr.Length, k);
+
+        for (var i = 0; i < shift; i++)
+        {
+            var list = GetIthList(i, shift, arr);
+            result += GetMin(list);
+        }
+
+        return result;
+    }
+
+    private static long GetMin(List<int> list)
+    {
+        list.Sort();
+        var val = list[list.Count / 2];
+
+        return list.Aggregate(0L, (current, n) => current + Math.Abs(n - val));
+    }
+
+    private static List<int> GetIthList(int index, int shift, int[] arr)
+    {
+        var result = new List<int>();
+        
+        for (var i = index; i < arr.Length; i += shift)
+            result.Add(arr[i]);
+
+        return result;
+    }
+
+    private static int GetMin(int n, int k) => n % k == 0 ? k : GetMin(k, n % k);
+}
