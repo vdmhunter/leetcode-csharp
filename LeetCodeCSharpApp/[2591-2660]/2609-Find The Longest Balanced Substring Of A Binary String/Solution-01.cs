6b0cdfd9004d6c1fa7f412e74cc4312ae583269d
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-04-02 20:35:17 +0300

    Add solution #01 for "2609. Find the Longest Balanced Substring of a Binary String"

---
+++ LeetCodeCSharpApp/[2591-2660]/2609-Find The Longest Balanced Substring Of A Binary String/Solution-01.cs
@@ -0,0 +1,28 @@
+namespace LeetCodeCSharpApp.FindTheLongestBalancedSubstringOfABinaryString01;
+
+public class Solution
+{
+    public int FindTheLongestBalancedSubstring(string s)
+    {
+        var result = 0;
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            for (var j = i; j < s.Length; j++)
+            {
+                var substr = s.Substring(i, j - i + 1);
+                var len = substr.Length;
+                
+                if (len % 2 == 0 &&
+                    substr[..(len / 2)].All(c => c == '0') &&
+                    substr[(len / 2)..].All(c => c == '1'))
+                {
+                    result = Math.Max(result, j - i + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
