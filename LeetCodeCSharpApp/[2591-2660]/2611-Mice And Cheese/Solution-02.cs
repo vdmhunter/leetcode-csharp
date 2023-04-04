395167d762d78a99af12c9eb688401ff78e0b922
Vladimir Dmitriev <dmitriev@ofd.ru>
2023-04-04 15:27:09 +0300

    Small refactoring for solution #02 for "2611. Mice and Cheese"

--- LeetCodeCSharpApp/[2591-2660]/2611-Mice And Cheese/Solution-02.cs
+++ LeetCodeCSharpApp/[2591-2660]/2611-Mice And Cheese/Solution-02.cs
@@ -4,6 +4,6 @@ public class Solution
 {
     public int MiceAndCheese(int[] reward1, int[] reward2, int k)
     {
-        return reward2.Sum() + (from p in reward1.Zip(reward2, (a, b) => a - b) orderby p descending select p).Take(k).Sum();
+        return reward2.Sum() + reward1.Zip(reward2, (a, b) => a - b).OrderByDescending(p => p).Take(k).Sum();
     }
 }
