182eac157c06a70eb782566c0f74387801822071
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-04-15 20:22:54 +0300

    Add Node class in LeetCodeCSharpApp.Common.CloneGraph namespace

---
+++ LeetCodeCSharpApp/Common/CloneGraph/Node.cs
@@ -0,0 +1,28 @@
+// ReSharper disable InconsistentNaming
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+
+namespace LeetCodeCSharpApp.Common.CloneGraph;
+
+public class Node
+{
+    public int val;
+    public IList<Node> neighbors;
+
+    public Node()
+    {
+        val = 0;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val)
+    {
+        val = _val;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val, List<Node> _neighbors)
+    {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
