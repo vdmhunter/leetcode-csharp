d734a675922ac86fd8770033e00ae4f8e5226fe2
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-04-16 16:40:13 +0300

    Add solution #01 for "2645. Minimum Additions to Make Valid String"

---
+++ LeetCodeCSharpApp/[2591-2660]/2645-Minimum Additions To Make Valid String/Solution-01.cs
@@ -0,0 +1,21 @@
+namespace LeetCodeCSharpApp.MinimumAdditionsToMakeValidString01;
+
+public class Solution
+{
+    public int AddMinimum(string word)
+    {
+        int cnt = 0, pos = 0;
+
+        foreach (var cur in word.Select(ch => ch - 'a'))
+        {
+            if (cur < pos)
+                ++cnt;
+
+            pos = cur + 1;
+        }
+
+        cnt += pos > 0 ? 1 : 0;
+        
+        return cnt * 3 - word.Length;
+    }
+}
