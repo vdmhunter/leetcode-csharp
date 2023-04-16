9732b824b00cfe444d015b04a05b045d738a89cc
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-04-16 16:38:41 +0300

    Add solution #01 for "2643. Row With Maximum Ones"

---
+++ LeetCodeCSharpApp/[2591-2660]/2643-Row With Maximum Ones/Solution-01.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeCSharpApp.RowWithMaximumOnes01;
+
+public class Solution
+{
+    public int[] RowAndMaximumOnes(int[][] mat)
+    {
+        var maxCount = 0;
+        var maxRowIndex = 0;
+
+        for (var i = 0; i < mat.Length; i++)
+        {
+            var count = 0;
+            
+            for (var j = 0; j < mat[i].Length; j++)
+                if (mat[i][j] == 1)
+                    count++;
+
+            if (count <= maxCount)
+                continue;
+
+            maxCount = count;
+            maxRowIndex = i;
+        }
+
+        return new[] { maxRowIndex, maxCount };
+    }
+}
