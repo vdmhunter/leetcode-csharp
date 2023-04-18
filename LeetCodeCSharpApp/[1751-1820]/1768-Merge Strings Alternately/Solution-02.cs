f7c109746ff67e3b4077d8822ec3cbad0057ac69
Vladimir Dmitriev <dmitriev@ofd.ru>
2023-04-18 13:02:40 +0300

    Small refactoring for solution #02 for "1768. Merge Strings Alternately"

--- LeetCodeCSharpApp/[1751-1820]/1768-Merge Strings Alternately/Solution-02.cs
+++ LeetCodeCSharpApp/[1751-1820]/1768-Merge Strings Alternately/Solution-02.cs
@@ -4,8 +4,7 @@ public class Solution
 {
     public string MergeAlternately(string word1, string word2)
     {
-        return string.Concat(word1.Zip(word2, (a, b) => $"{a}{b}").SelectMany(x => x)) +
-               (word1.Length > word2.Length ? word1[word2.Length..] : "") +
-               (word2.Length > word1.Length ? word2[word1.Length..] : "");
+        return string.Concat(word1.Zip(word2, (a, b) => $"{a}{b}")) +
+               (word1.Length > word2.Length ? word1[word2.Length..] : word2[word1.Length..]);
     }
 }
