9976eab9e0a337c9e356a3d0bd9ff9ddbe5d66b5
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-05-04 17:20:18 +0300

    Small refactoring for "576. Out of Boundary Paths" (solution #03)

--- LeetCodeCSharpApp/[0561-0630]/0576-Out Of Boundary Paths/Solution-03.cs
+++ LeetCodeCSharpApp/[0561-0630]/0576-Out Of Boundary Paths/Solution-03.cs
@@ -5,7 +5,7 @@ namespace LeetCodeCSharpApp.OutOfBoundaryPaths03;
 /// </summary>
 public class Solution
 {
-    private const int M = 1000000007;
+    private const int Mod = 1_000_000_007;
 
     public int FindPaths(int m, int n, int maxMove, int startRow, int startColumn)
     {
@@ -20,28 +20,38 @@ public class Solution
             for (var i = 0; i < m; i++)
                 for (var j = 0; j < n; j++)
                 {
-                    if (i == m - 1)
-                        count = (count + dp[i, j]) % M;
-
-                    if (j == n - 1)
-                        count = (count + dp[i, j]) % M;
-
-                    if (i == 0)
-                        count = (count + dp[i, j]) % M;
-
-                    if (j == 0)
-                        count = (count + dp[i, j]) % M;
-
-                    temp[i, j] =
-                    (
-                        ((i > 0 ? dp[i - 1, j] : 0) + (i < m - 1 ? dp[i + 1, j] : 0)) % M +
-                        ((j > 0 ? dp[i, j - 1] : 0) + (j < n - 1 ? dp[i, j + 1] : 0)) % M
-                    ) % M;
+                    count = UpdateCount(count, dp, i, j, m, n);
+                    temp[i, j] = UpdateTemp(dp, i, j, m, n);
                 }
 
             dp = temp;
         }
 
         return count;
+    }
+
+    private static int UpdateCount(int count, int[,] dp, int i, int j, int m, int n)
+    {
+        if (i == m - 1)
+            count = (count + dp[i, j]) % Mod;
+
+        if (j == n - 1)
+            count = (count + dp[i, j]) % Mod;
+
+        if (i == 0)
+            count = (count + dp[i, j]) % Mod;
+
+        if (j == 0)
+            count = (count + dp[i, j]) % Mod;
+
+        return count;
+    }
+
+    private static int UpdateTemp(int[,] dp, int i, int j, int m, int n)
+    {
+        return (
+            ((i > 0 ? dp[i - 1, j] : 0) + (i < m - 1 ? dp[i + 1, j] : 0)) % Mod +
+            ((j > 0 ? dp[i, j - 1] : 0) + (j < n - 1 ? dp[i, j + 1] : 0)) % Mod
+        ) % Mod;
     }
 }
