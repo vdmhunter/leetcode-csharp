d6e1cff363f9eeb6896f3ab1903dfe0be8b7f9e1
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-05-13 23:09:16 +0300

    Small refactoring for "1162. As Far from Land as Possible" (solution #01)

--- LeetCodeCSharpApp/[1121-1190]/1162-As Far From Land As Possible/Solution-01.cs
+++ LeetCodeCSharpApp/[1121-1190]/1162-As Far From Land As Possible/Solution-01.cs
@@ -5,42 +5,54 @@ public class Solution
     public int MaxDistance(int[][] grid)
     {
         var length = grid.Length;
+        var dp = InitializeDP(grid, length);
+
+        UpdateDPFromTopLeft(grid, length, dp);
+        var result = UpdateDPFromBottomRight(grid, length, dp);
+
+        return result == 0 ? -1 : result;
+    }
+
+    private static int[,] InitializeDP(int[][] grid, int length)
+    {
         var dp = new int[length, length];
 
         for (var i = 0; i < length; i++)
             for (var j = 0; j < length; j++)
                 dp[i, j] = grid[i][j] == 1 ? 0 : int.MaxValue;
 
+        return dp;
+    }
+
+    private static void UpdateDPFromTopLeft(int[][] grid, int length, int[,] dp)
+    {
         for (var i = 0; i < length; i++)
             for (var j = 0; j < length; j++)
                 if (grid[i][j] == 0)
-                {
-                    if (j > 0 && dp[i, j - 1] != int.MaxValue)
-                        dp[i, j] = Math.Min(dp[i, j], 1 + dp[i, j - 1]);
+                    UpdateDP(dp, i, j, -1);
+    }
 
-                    if (i > 0 && dp[i - 1, j] != int.MaxValue)
-                        dp[i, j] = Math.Min(dp[i, j], 1 + dp[i - 1, j]);
-                }
-
+    private static int UpdateDPFromBottomRight(int[][] grid, int length, int[,] dp)
+    {
         var result = 0;
 
         for (var i = length - 1; i >= 0; i--)
             for (var j = length - 1; j >= 0; j--)
                 if (grid[i][j] == 0)
                 {
-                    if (j < length - 1 && dp[i, j + 1] != int.MaxValue)
-                        dp[i, j] = Math.Min(dp[i, j], 1 + dp[i, j + 1]);
-
-                    if (i < length - 1 && dp[i + 1, j] != int.MaxValue)
-                        dp[i, j] = Math.Min(dp[i, j], 1 + dp[i + 1, j]);
-
-                    if (dp[i, j] != int.MaxValue && result < dp[i, j])
-                        result = dp[i, j];
+                    UpdateDP(dp, i, j, 1);
+                    result = Math.Max(result, dp[i, j]);
                 }
 
-        if (result == 0)
-            return -1;
+        return result == int.MaxValue ? 0 : result;
+    }
 
-        return result;
+    private static void UpdateDP(int[,] dp, int i, int j, int offset)
+    {
+        if (j + offset >= 0 && j + offset < dp.GetLength(1) && dp[i, j + offset] != int.MaxValue)
+            dp[i, j] = Math.Min(dp[i, j], 1 + dp[i, j + offset]);
+
+        if (i + offset >= 0 && i + offset < dp.GetLength(0) && dp[i + offset, j] != int.MaxValue)
+            dp[i, j] = Math.Min(dp[i, j], 1 + dp[i + offset, j]);
     }
 }
