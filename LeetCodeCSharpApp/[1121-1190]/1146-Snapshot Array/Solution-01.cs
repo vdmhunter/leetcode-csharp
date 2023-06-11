0c5d47c8d2aaf57bbf6626556094df4caf3c02e4
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-06-11 17:43:39 +0300

    Add solution #01 for "1146. Snapshot Array"

---
+++ LeetCodeCSharpApp/[1121-1190]/1146-Snapshot Array/Solution-01.cs
@@ -0,0 +1,35 @@
+namespace LeetCodeCSharpApp.SnapshotArray01;
+
+public class SnapshotArray
+{
+    private readonly List<Dictionary<int, int>> _array;
+    private int _snapshotId;
+
+    public SnapshotArray(int length)
+    {
+        _array = new List<Dictionary<int, int>>(length);
+
+        for (var i = 0; i < length; i++)
+            _array.Add(new Dictionary<int, int> { [0] = 0 });
+    }
+
+    public void Set(int index, int val)
+    {
+        _array[index][_snapshotId] = val;
+    }
+
+    public int Snap()
+    {
+        return _snapshotId++;
+    }
+
+    public int Get(int index, int snapId)
+    {
+        var dictionary = _array[index];
+
+        while (!dictionary.ContainsKey(snapId))
+            --snapId;
+
+        return dictionary[snapId];
+    }
+}
