904000697888adb973046bd0ee0cba4ad0c5840b
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-07-09 12:05:34 +0300

    Add solution #01 for "2769. Find the Maximum Achievable Number"

---
+++ LeetCodeCSharpApp/[2731-2800]/2769-Find The Maximum Achievable Number/Solution-01.cs
@@ -0,0 +1,9 @@
+namespace LeetCodeCSharpApp.FindTheMaximumAchievableNumber01;
+
+public class Solution
+{
+    public int TheMaximumAchievableX(int num, int t)
+    {
+        return num + 2 * t;
+    }
+}
