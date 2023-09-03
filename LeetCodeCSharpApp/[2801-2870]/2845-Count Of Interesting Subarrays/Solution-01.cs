4f6d7cbe69eeda78bce9ef796bd92336c0dbf6ce
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-09-04 02:35:50 +0300

    Add solution #01 for "2845. Count of Interesting Subarrays"

---
+++ LeetCodeCSharpApp/[2801-2870]/2845-Count Of Interesting Subarrays/Solution-01.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeCSharpApp.CountOfInterestingSubarrays01;
+
+public class Solution
+{
+    public long CountInterestingSubarrays(IList<int> nums, int modulo, int k)
+    {
+        var result = 0L;
+        var sum = 0;
+        var map = new Dictionary<int, int> { [0] = 1 };
+
+        foreach (var a in nums)
+        {
+            sum = (sum + (a % modulo == k ? 1 : 0)) % modulo;
+
+            result += map.ContainsKey((sum - k + modulo) % modulo)
+                ? map[(sum - k + modulo) % modulo]
+                : 0;
+
+            if (map.ContainsKey(sum))
+                map[sum]++;
+            else
+                map[sum] = 1;
+        }
+
+        return result;
+    }
+}
