5d2bef2a21e24d2544aa0cba6303e8aa010517b9
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-10-25 22:09:20 +0300

    Add solution #01 for "779. K-th Symbol in Grammar"

---
+++ LeetCodeCSharpApp/[0771-0840]/0779-K-th Symbol In Grammar/Solution-01.cs
@@ -0,0 +1,15 @@
+namespace LeetCodeCSharpApp.KthSymbolInGrammar01;
+
+public class Solution
+{
+    public int KthGrammar(int n, int k)
+    {
+        if (n == 1)
+            return 0;
+
+        if (k % 2 == 0)
+            return KthGrammar(n - 1, k / 2) == 0 ? 1 : 0;
+
+        return KthGrammar(n - 1, (k + 1) / 2) == 0 ? 0 : 1;
+    }
+}
