3e7baa370968e082bc9d61caae423a3f3bb6c7c8
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-11-15 23:27:50 +0300

    Small refactoring for "2932. Maximum Strong Pair XOR I" (solution #01)

--- LeetCodeCSharpApp/[2871-2940]/2932-Maximum Strong Pair XOR I/Solution-01.cs
+++ LeetCodeCSharpApp/[2871-2940]/2932-Maximum Strong Pair XOR I/Solution-01.cs
@@ -4,7 +4,7 @@ public class Solution
 {
     public int MaximumStrongPairXor(int[] nums)
     {
-        return nums.SelectMany(x => nums, (x, y) => new { x, y })
+        return nums.SelectMany(_ => nums, (x, y) => new { x, y })
             .Where(pair => Math.Abs(pair.x - pair.y) <= Math.Min(pair.x, pair.y))
             .Max(pair => pair.x ^ pair.y);
     }
