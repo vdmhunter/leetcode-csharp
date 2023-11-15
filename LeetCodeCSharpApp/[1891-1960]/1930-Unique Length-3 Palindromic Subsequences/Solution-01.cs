7305486fb5527070dcef909380ace37b47b76a77
Vladimir Dmitriev <vdmhunter@gmail.com>
2023-11-15 17:15:52 +0300

    Add solution #01 for "1930. Unique Length-3 Palindromic Subsequences"

---
+++ LeetCodeCSharpApp/[1891-1960]/1930-Unique Length-3 Palindromic Subsequences/Solution-01.cs
@@ -0,0 +1,27 @@
+namespace LeetCodeCSharpApp.UniqueLength3PalindromicSubsequences01;
+
+public class Solution
+{
+    public int CountPalindromicSubsequence(string s)
+    {
+        var first = new int[26];
+        var last = new int[26];
+        var result = 0;
+
+        Array.Fill(first, int.MaxValue);
+
+        for (var i = 0; i < s.Length; ++i)
+        {
+            first[s[i] - 'a'] = Math.Min(first[s[i] - 'a'], i);
+            last[s[i] - 'a'] = i;
+        }
+
+        for (var i = 0; i < 26; ++i)
+            if (first[i] < last[i])
+                result += s.Substring(first[i] + 1, last[i] - first[i] - 1)
+                    .Distinct()
+                    .Count();
+
+        return result;
+    }
+}
