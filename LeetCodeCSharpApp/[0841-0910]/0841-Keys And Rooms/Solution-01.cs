fae24ecb52ddf4a875528e0d82d5506bc84e97fc
Vladimir Dmitriev <dmitriev@ofd.ru>
2024-01-25 16:54:19 +0300

    Small refactoring for solution #01 for "841. Keys and Rooms"

--- LeetCodeCSharpApp/[0841-0910]/0841-Keys And Rooms/Solution-01.cs
+++ LeetCodeCSharpApp/[0841-0910]/0841-Keys And Rooms/Solution-01.cs
@@ -1,45 +1,26 @@
 namespace LeetCodeCSharpApp.KeysAndRooms01;
 
-/// <summary>
-/// BFS + Hash Set
-/// </summary>
 public class Solution
 {
     public bool CanVisitAllRooms(IList<IList<int>> rooms)
     {
-        // Edge cases.
-        if (!rooms.Any())
-            return false;
-        if (!rooms[0].Any())
+        if (rooms.Count == 0 || rooms[0].Count == 0)
             return false;
 
-        // Keep track of rooms we already checked.
-        HashSet<int> searchedRooms = new();
-
-        // Each integer represents the next room(s) to check.
-        Queue<int> queue = new();
-
-        // First-time setup. According to the problem, we start in the 0th room.
+        var visitedRooms = new HashSet<int>();
+        var queue = new Queue<int>();
         queue.Enqueue(0);
 
-        while (queue.Any())
-            for (var i = 0; i < queue.Count; i++)
-            {
-                var room = queue.Dequeue();
-                searchedRooms.Add(room);
-
-                // Take all keys in this room and search those next rooms.
-                foreach (var key in rooms[room])
-                    // If this key belongs to a room we already searched, skip it.
-                    CheckRoom(key);
-            }
-
-        void CheckRoom(int key1)
+        while (queue.Count > 0)
         {
-            if (!searchedRooms.Contains(key1))
-                queue.Enqueue(key1);
+            var room = queue.Dequeue();
+            visitedRooms.Add(room);
+
+            foreach (var key in rooms[room])
+                if (!visitedRooms.Contains(key))
+                    queue.Enqueue(key);
         }
 
-        return searchedRooms.Count == rooms.Count;
+        return visitedRooms.Count == rooms.Count;
     }
 }
